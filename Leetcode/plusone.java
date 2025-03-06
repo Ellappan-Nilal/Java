f7f5497579b9da6dbb343ab75ba010a4c500ad89
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-03-06 23:19:19 +0530

    Substring and plusone program added

---
+++ Leetcode/plusone.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Nilal
+ */
+public class plusone {
+    public static void main(String[] args){
+        int[] digits={1,2,3};
+        System.out.println(Arrays.toString(plusone(digits)));
+    }
+    static int[] plusone(int[] digits) {
+        int n=digits.length;
+        for(int i=n-1;i>=0;i--){
+           if(digits[i]<9) {
+               digits[i]++;
+               return digits;
+           }
+           digits[i]=0;
+        }
+        int[] newNo=new int[n+1];
+        newNo[0]=1;
+        return newNo;
+     }
+}
