fc569c0b1d71e730971d0c0908efedae573f55fd
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-03-07 23:08:34 +0530

    files are changed

--- HackerRank/sub_string_compression.java
+++ HackerRank/sub_string_compression.java
@@ -2,22 +2,21 @@ import java.util.Scanner;
 
 public class sub_string_compression {
 
-    public static String getSmallestAndLargest(String s, int k) {
-        String currentSubstring=s.substring(0,k);
-        String smallest = "";
-        String largest = "";
-        for(int i=1;i<=s.length()-k;i++){
-            if(s.substring(i,i+k).compareTo(largest)>0){
-                largest=s.substring(i,i+k);
+   public static String getSmallestAndLargest(String s, int k) {
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        for (int i = 1; i <= s.length() - k; i++) {
+            String currentSubstring = s.substring(i, i + k);
+            if (currentSubstring.compareTo(largest) > 0) {
+                largest = currentSubstring;
             }
-            if(s.substring(i,i+k).compareTo(smallest)<0){
-                smallest=s.substring(i,i+k);
+            if (currentSubstring.compareTo(smallest) < 0) {
+                smallest = currentSubstring;
             }
         }
-        
         return smallest + "\n" + largest;
-    }
-
+   }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
---
+++ Leetcode/Add_binary.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode;
+
+/**
+ *
+ * @author Nilal
+ */
+public class Add_binary {
+    public static void main(String[] args){
+        String a="11";String b="1";
+        System.out.println(addBinary(a,b));
+    }
+    public static String addBinary(String a,String b){
+        StringBuilder sb=new StringBuilder();
+        int i=a.length()-1;
+        int j=b.length()-1;
+        int carry=0;
+        while(i>=0 || j>=0|| carry==1){
+            int sum=carry;
+            if(i>=0){
+                sum=sum+a.charAt(i)-'0';
+                i--;
+            }
+            if(j>=0){
+                sum=sum+b.charAt(j)-'0';
+                j--;
+            }
+            sb.append(sum%2);
+            carry=sum/2;
+        }
+        return sb.reverse().toString();
+    }
+}
