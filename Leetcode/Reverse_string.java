397a5eb08ec25041088226fc755e800b1f4fcf6f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-03-12 23:24:32 +0530

    Reverse file is added

---
+++ Leetcode/Reverse_string.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode;
+
+/**
+ *
+ * @author Nilal
+ */
+public class Reverse_string {
+    
+    public void reverseString(char[] s) {
+        int left=0,right=s.length-1;
+        while(left<right){
+            char temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+            left++;
+            right--;
+        }
+
+    }
+}
+
+
