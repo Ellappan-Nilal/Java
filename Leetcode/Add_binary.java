e42b2c8382850c0e19d0b09c759a8e2a673309e9
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-03-14 23:37:28 +0530

    Modifies

--- Leetcode/Add_binary.java
+++ Leetcode/Add_binary.java
@@ -1,13 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package solve_problems.Leetcode;
 
-/**
- *
- * @author Nilal
- */
 public class Add_binary {
     public static void main(String[] args){
         String a="11";String b="1";
