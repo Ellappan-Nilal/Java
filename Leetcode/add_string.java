a30ca58224270f5cc9991f37bf44bd5beb37f0b4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-03-22 23:23:39 +0530

    Add string

---
+++ Leetcode/add_string.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode;
+
+/**
+ *
+ * @author Nilal
+ */
+public class add_string {
+   class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder str = new StringBuilder();
+
+        int i = num1.length() - 1, j = num2.length() - 1, carry = 0, sum;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int digit1 = (i >= 0) ? num1.charAt(i--) - '0' : 0;
+            int digit2 = (j >= 0) ? num2.charAt(j--) - '0' : 0;
+
+            sum = digit1 + digit2 + carry;
+            str.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return str.reverse().toString();
+    }
+}
+}
