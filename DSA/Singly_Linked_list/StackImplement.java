24be10092b6e7d5b0a33caabc33007d613fe051e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-14 19:47:59 +0530

    Stack Implement

---
+++ DSA/Singly_Linked_list/StackImplement.java
@@ -0,0 +1,13 @@
+//import java.util.Stack;in build method
+
+public class StackImplement {
+    public static void main(String[] args){
+        Stack nums=new Stack();
+        nums.push(3);
+        nums.push(5);
+        nums.push(38);
+        nums.push(30);
+        nums.push(35);
+        nums.printStack();
+    }
+}
