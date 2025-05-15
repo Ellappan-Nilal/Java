f3684011262d845d68a34abea95b2121483d8af8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-15 19:15:30 +0530

    Stack pop and peak operation can be added

--- DSA/Singly_Linked_list/StackImplement.java
+++ DSA/Singly_Linked_list/StackImplement.java
@@ -6,8 +6,11 @@ public class StackImplement {
         nums.push(3);
         nums.push(5);
         nums.push(38);
+        System.out.println(nums.pop());
         nums.push(30);
         nums.push(35);
+        System.out.println(nums.peak());
+        nums.push(67);
         nums.printStack();
     }
 }
