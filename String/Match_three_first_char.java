0027bd7def024c185e2c838f9bd0f03074fc3644
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-17 22:34:36 +0530

    Codeded

--- String/Match_three_first_char.java
+++ String/Match_three_first_char.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Match_three_first_char {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        
+        String st1=sc.next();
+        String st2=sc.next();
+        if(st1.charAt(0)==st2.charAt(0) && st1.charAt(1)==st2.charAt(1) && st1.charAt(3)==st2.charAt(3)){
+            System.out.print("Yes");
+        }else{
+            System.out.print("No");
+        }
     }
 }
