f18e9481a6b24d9cac571a50ddaea752791177c5
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-20 23:02:49 +0530

    Create Count_of_characters.java
    Count of the characters

---
+++ String/Count_of_characters.java
@@ -0,0 +1,12 @@
+public class Count_of_characters{
+  public static void main(String[] args){
+    String str="Hello World";
+    int count=0;
+    for(int i=0;i<str.length();i++){
+      if(str.charAt(i)!=' '){
+        count++;
+      }
+    }
+    System.out.println("number of characters in a string are"+count);
+  }
+}
