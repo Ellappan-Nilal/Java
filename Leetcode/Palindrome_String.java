56f50c85f029111b0af8a33fe5f39134c712a4f1
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-20 21:38:49 +0530

    Create Palindrome_String.java
    String palindrome in java

---
+++ Leetcode/Palindrome_String.java
@@ -0,0 +1,15 @@
+public class Palindrome_String{
+  public static void main(String[] args){
+    String str="Level";
+    String rev="";
+    for(int i=str.length()-1;i>=0;i--){
+      rev=rev+str.charAt(i);
+    }
+    if(str.toLowercase().equals(rev.toowerCase())){
+      System.out.println("Palindrome String");
+    }
+    else{
+      System.out.println("Not palindrome");
+    }
+  }
+}
