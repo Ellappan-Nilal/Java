fc566601fc08612f6f1be789db537a140e11ba52
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-20 21:19:53 +0530

    Create Reverse_words_in_a_string.java
    Reverse words in a string pro 151

---
+++ Leetcode/Reverse_words_in_a_string.java
@@ -0,0 +1,15 @@
+public class Reverse_words_in_a_string{
+  public static void main(String[] args){
+    System.out.println(reverWords("the sky is blue"));
+  }
+}
+
+
+static String reverseWords(String s){
+  String[] words=s.split(" +");
+  StringBuilder sb=new StringBuilder();
+  for(int i=words.length-1;i>=0;i--){
+      sb.append(" ");
+  }
+  return sb.toString().trim();
+}
