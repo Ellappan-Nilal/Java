d8f258f60e68353c89d9b3c890448101e4187414
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-21 22:18:55 +0530

    Five interger odd counter

--- Number_Series/First_three_factors.java
+++ Number_Series/First_three_factors.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solve_problems.Number_Series;
-
-/**
- *
- * @author Nilal
- */
+import java.util.Scanner;
 public class First_three_factors {
-    
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int factor=0,ctr=1;
+        while(factor<3){
+            if(n%ctr==0){
+                System.out.print(ctr+" ");
+                factor++;
+            }
+            ctr++;
+        }
+    }
 }
