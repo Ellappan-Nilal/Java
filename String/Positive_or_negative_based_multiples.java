1c8d9de4947d2c117cabea59a9fc50d0cf5d8720
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-24 22:15:17 +0530

    positive or negative based multiples

---
+++ String/Positive_or_negative_based_multiples.java
@@ -0,0 +1,10 @@
+package solve_problems.String;
+import java.util.Scanner;
+public class Positive_or_negative_based_multiples {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ctr,value;
+        
+    }
+}
