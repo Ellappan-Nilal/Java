59c8797acdf919f07279117d95ba088f07208931
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-24 22:20:36 +0530

    Files changes

--- String/Positive_or_negative_based_multiples.java
+++ String/Positive_or_negative_based_multiples.java
@@ -5,6 +5,17 @@ public class Positive_or_negative_based_multiples {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int ctr,value;
+        if(n>0){
+            value=n%10;
+            for(ctr=1;ctr<=5;ctr++){
+                System.out.print(value*ctr+" ");
+            }
+        }else{
+            value=(n%10)*-1;
+            for(ctr=1;ctr<=10;ctr++){
+                System.out.print(value*ctr+" ");
+            }
+        }
         
     }
 }
