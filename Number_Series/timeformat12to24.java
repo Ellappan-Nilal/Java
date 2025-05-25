9e60508b042aff99d0e0efd2effef20a5d824021
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-25 21:49:20 +0530

    change

--- Number_Series/timeformat12to24.java
+++ Number_Series/timeformat12to24.java
@@ -3,6 +3,19 @@ import java.util.Scanner;
 public class timeformat12to24 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int time
+        int time=sc.nextInt();
+        int hours,mintues;
+        String period="AM";
+        hours=time/100;
+        mintues=time%100;
+        if(hours==0){
+            hours=12;
+        }else if(hours==12){
+            period=period.replace('A', 'P');
+        }else if(hours>12){
+            hours-=12;
+            period=period.replace('A', 'P');
+        }
+        System.out.printf("%02d:%02d %s",hours,mintues,period);
     }
 }
