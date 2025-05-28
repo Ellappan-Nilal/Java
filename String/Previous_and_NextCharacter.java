94dad5378d5c4151ca06e6e72ecb4251d9926fdf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-28 22:24:31 +0530

    Files change

--- String/Previous_and_NextCharacter.java
+++ String/Previous_and_NextCharacter.java
@@ -2,7 +2,7 @@ package solve_problems.String;
 import java.util.Scanner;
 public class Previous_and_NextCharacter {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        
         char ch=sc.next().charAt(0);
         if(ch=='a'){
             System.out.print("z" + ++ch);
