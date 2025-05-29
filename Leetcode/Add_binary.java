39e7d089215f4d7cc491068919b31f6505f34625
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-29 22:21:00 +0530

    files change

--- Leetcode/Add_binary.java
+++ Leetcode/Add_binary.java
@@ -5,7 +5,9 @@ public class Add_binary {
         System.out.println(addBinary(a,b));
     }
     public static String addBinary(String a,String b){
-       
+        StringBuilder sb=new StringBuilder();
+        int i=a.length()-1;
+        int j=b.length()-1;
         int carry=0;
         while(i>=0 || j>=0|| carry==1){
             int sum=carry;
