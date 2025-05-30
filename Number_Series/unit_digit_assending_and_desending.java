e3fd5edf58b6a87efd7562669f2cb0bf8a939192
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-30 22:31:01 +0530

    unit digit assending and desending

---
+++ Number_Series/unit_digit_assending_and_desending.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Number_Series;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nilal
+ */
+public class unit_digit_assending_and_desending {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int unidix=x%10;
+        int unidiy=y%10;
+        if(unidix==unidiy){
+            if(x>y){
+                System.out.print(x+" "+y);
+            }else{
+                System.out.print(y+" "+x);
+            }
+        }
+        else if(unidix < unidiy){
+          System.out.print(y+" "+x);
+        }
+        else{
+            System.out.print(y+" "+x);
+        }
+    }
+}
