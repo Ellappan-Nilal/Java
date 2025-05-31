523f0edb31ebac236aef7ce749dca16735fc97cf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-05-31 23:14:30 +0530

    Reverse even integers

--- Number_Series/Reverse_even_integers.java
+++ Number_Series/Reverse_even_integers.java
@@ -1,31 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.Number_Series;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Nilal
- */
-public class Two_digit_number_print {
+public class Reverse_even_integers {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
-        int printed=0;
+        boolean pri=false;
         for(int i=0;i<n;i++){
             arr[i]=sc.nextInt();
         }
         for(int i=n-1;i>=0;i--){
-            if(arr[i]>=10&&arr[i]<=99){
+            if(arr[i]%2==0){
                 System.out.print(arr[i]+" ");
-                printed=1;
+                pri=true;
             }
         }
-        if(printed==0){
+        if(!pri){
             System.out.print("-1");
         }
     }
