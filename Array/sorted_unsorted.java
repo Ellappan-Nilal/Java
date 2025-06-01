3412dcc307bc1dc1f7b997b5ebb4ccab5e5caef8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-01 22:02:12 +0530

    file change

--- Array/sorted_unsorted.java
+++ Array/sorted_unsorted.java
@@ -2,7 +2,9 @@
 import java.util.Scanner;
 public class sorted_unsorted {
     public static void main(String[] args){
-       
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] a=new int[n];
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
         }
