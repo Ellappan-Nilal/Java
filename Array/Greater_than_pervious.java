f226132b8ddfb5e76c87b9f56289821ac8f9983f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-02 23:20:14 +0530

    file schange

--- Array/Greater_than_pervious.java
+++ Array/Greater_than_pervious.java
@@ -1,7 +1,8 @@
 import java.util.*;
 public class Greater_than_pervious {
     public static void main(String[] args){
-       
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
         int[] a=new int[n];
         for(int i=0;i<n;i++){
             a[i]=sc.nextInt();
