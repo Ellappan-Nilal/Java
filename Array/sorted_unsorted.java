4e2fc20f516570890e71632177b013c30772a17e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-02 23:15:09 +0530

    File

--- Array/sorted_unsorted.java
+++ Array/sorted_unsorted.java
@@ -3,9 +3,7 @@ import java.util.Scanner;
 public class sorted_unsorted {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        int n=sc.nextInt();
-        int[] a=new int[n];
-        for(int i=0;i<n;i++){
+        
             a[i]=sc.nextInt();
         }
         boolean bool=true;
