c72b3d888951683cc1db22d55a0b084f0be2dc44
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-03 22:20:33 +0530

    File change

--- Matrix/Maximum_element_in_column.java
+++ Matrix/Maximum_element_in_column.java
@@ -1,9 +1,7 @@
 import java.util.*;
 public class Maximum_element_in_column {
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
-        int r=sc.nextInt();
-        int c=sc.nextInt();
+       
         int[][] mat=new int[r][c];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
