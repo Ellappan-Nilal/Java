54abf6d0cae2ac0282dd4b8eda880efc6d25b481
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-04 22:50:58 +0530

    file change

--- String/Basic.java
+++ String/Basic.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Basic {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
-        
+        String s=sc.nextLine();
+        System.out.println(s);
         int cnt=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)!=' '){
