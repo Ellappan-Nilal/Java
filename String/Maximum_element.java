228acf00c2b67b4f607c1c48778496f8764911a6
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-15 20:25:59 +0530

    file added

--- String/Maximum_element.java
+++ String/Maximum_element.java
@@ -3,7 +3,7 @@ public class Maximum_element {
     public static void main(String[] args){
         Scanner sc= new Scanner(System.in);
         int n=sc.nextInt();
-       
+        System.out.print(n);
 
     }
 }
