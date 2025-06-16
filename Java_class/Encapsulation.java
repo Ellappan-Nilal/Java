cf472f687bb86189d9a45a815d720602d4a739be
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-16 19:21:46 +0530

    encapsultion code is added

--- Java_class/Encapsulation.java
+++ Java_class/Encapsulation.java
@@ -8,6 +8,49 @@ package solve_problems.Java_class;
  *
  * @author Nilal
  */
-public class Encapsulation {
+class Account{
+    private int acc_no;
+    private String name,email;
+    private float amount;
+
+    public int getAcc_no() {
+        return acc_no;
+    }
+
+    public void setAcc_no(int acc_no) {
+        this.acc_no = acc_no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
     
+    
+}
+public class Encapsulation { 
+public static void main(String[] args){
+    Account ac=new Account();
+    ac.setAcc_no(12345);
+    System.out.println(ac.getAcc_no());
+}
 }
