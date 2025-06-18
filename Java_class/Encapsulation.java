d00f09cde95c7974de2836f06d3803b2a180363a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-06-18 21:07:44 +0530

    file change

--- Java_class/Encapsulation.java
+++ Java_class/Encapsulation.java
@@ -10,7 +10,9 @@ package solve_problems.Java_class;
  */
 class Account {
 
-    
+    private int acc_no;
+    private String name, email;
+    private float amount;
 
     public int getAcc_no() {
         return acc_no;
