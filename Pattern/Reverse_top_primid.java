d78c1a3caef3fc5d956e9aae34247c1baeba156e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-08 21:48:51 +0530

    Files are added

---
+++ Pattern/Reverse_top_primid.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Slove_problems.Pattern;
+
+import java.util.Scanner;
+
+/*****
+ ****
+ ***
+ **
+ */
+public class Reverse_top_primid {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern/Triangle_pattern.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Slove_problems.Pattern;
+
+import java.util.Scanner;
+
+/*    *
+    *   *
+   *  *  *
+  
+ */
+public class Triangle_pattern {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j++){
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
