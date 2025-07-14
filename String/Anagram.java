a669d8523db0316114194cfdba98626148c52a0a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-14 20:57:13 +0530

    file change

--- String/Anagram.java
+++ String/Anagram.java
@@ -17,7 +17,8 @@ public class Anagram {
     }
 
     public static void main(String[] args) {
-        
+        String str1 = "listen";
+        String str2 = "silent";
         String str3 = "hello";
         String str4 = "world";
 
