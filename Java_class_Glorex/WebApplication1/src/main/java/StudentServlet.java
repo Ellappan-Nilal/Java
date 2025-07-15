013a90acf2b6de38a69720af2fd54b8f4c629077
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-15 21:57:26 +0530

    file is browser println added

--- Java_class_Glorex/WebApplication1/src/main/java/StudentServlet.java
+++ Java_class_Glorex/WebApplication1/src/main/java/StudentServlet.java
@@ -18,7 +18,8 @@ public class StudentServlet extends HttpServlet {
        String std_name=request.getParameter("sname");
        String std_mail=request.getParameter("smail");
        String str_mob=request.getParameter("smob");
-       System.out.println(std_name+" "+std_mail+" "+str_mob);
+       PrintWriter out = response.getWriter();
+       out.println(std_name+" "+std_mail+" "+str_mob);
     }
 
     
