28d848348ed40784a0988ccd94312c7e1dc94a25
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-16 23:02:05 +0530

    file change

--- String/count_the_words.java
+++ String/count_the_words.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class count_the_words {
     public static void main(String[] args){
-      
+        Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
-        System.out.println(s);
+        System.out.println(s); 
         int cnt=0;
         for(int i=0;i<s.length();i++){
             if(s.charAt(i)==' '){
