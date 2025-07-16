43f4852a9a51aa6dd19ea975cabf94ca9cf75100
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-16 23:00:32 +0530

    file is chnage

--- String/count_the_words.java
+++ String/count_the_words.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class count_the_words {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+      
         String s=sc.nextLine();
         System.out.println(s);
         int cnt=0;
