8179f4964a4aa5972933d98155df200209fe13ab
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-18 22:04:57 +0530

    File is change

--- Pattern/Character/Even_character.java
+++ Pattern/Character/Even_character.java
@@ -18,9 +18,7 @@ public static void main(String[] args){
         int n=sc.nextInt();
         char c='B';
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=n-i;j++){
-                System.out.print(" ");
-            }
+           
             for(int k=1;k<=i;k++){
                 System.out.print(c+" ");
                 c+=2;
