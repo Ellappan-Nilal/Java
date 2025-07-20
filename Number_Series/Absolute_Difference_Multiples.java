b7c86c99a61cad6b44281844a57d70088b3b51ad
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-20 21:03:51 +0530

    File is change

--- Number_Series/Absolute_Difference_Multiples.java
+++ Number_Series/Absolute_Difference_Multiples.java
@@ -3,9 +3,7 @@ package solve_problems.Number_Series;
 import java.util.Scanner;
 public class Absolute_Difference_Multiples {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        int n1=sc.nextInt();
-        int n2=sc.nextInt();        
+            
         int diff=n1-n2;
         if(diff<0){
             diff=diff*(-1);
