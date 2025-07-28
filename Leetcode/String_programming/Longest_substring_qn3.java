9614b5f8f8ccd7875388ce16938e92ee9342cefd
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-07-28 22:40:02 +0530

    Longest substring qno 3

---
+++ Leetcode/String_programming/Longest_substring_qn3.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode.String_programming;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Nilal
+ */
+public class Longest_substring_qn3 {
+    public static void main(String[] args){
+        String s="abcdabcbb";
+        int start=0;
+        int end=0;
+        int max_length=0;
+        List<Character> list=new ArrayList<Character>();
+        while(end<s.length()){
+            if(!list.contains(s.charAt(end))){
+                list.add(s.charAt(end));
+                end++;
+                max_length=Math.max(max_length,list.size());
+            }
+            else{
+                list.remove(Character.valueOf(s.charAt(start)));
+                start++;
+            }
+        }
+        System.out.println("Max length is"+max_length);
+    }
+}
