57478a7da261ae08afac91822d2032a0d5771227
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-02 11:47:43 +0530

    JDBC connection file is added

--- Java_class_Glorex/WebApplication1/src/main/java/StudentServlet.java
+++ Java_class_Glorex/WebApplication1/src/main/java/StudentServlet.java
@@ -5,6 +5,12 @@
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,15 +19,33 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(urlPatterns = {"/StudentServlet"})
 public class StudentServlet extends HttpServlet {
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       String std_name=request.getParameter("sname");
-       String std_mail=request.getParameter("smail");
-       String str_mob=request.getParameter("smob");
-       PrintWriter out = response.getWriter();
-       out.println(std_name+" "+std_mail+" "+str_mob);
+        String std_name = request.getParameter("sname");
+        String std_mail = request.getParameter("smail");
+        String str_mob = request.getParameter("smob");
+        PrintWriter out = response.getWriter();
+        //Java Database connection 
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/webjava", "root", "");
+            String sql = "INSERT INTO studentdetails(name,email,phone) values(?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, std_name);
+            ps.setString(2, std_mail);
+            ps.setString(3, str_mob);
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+               // out.print("Successfully Inserted");
+               response.sendRedirect("Studentform.jsp");
+            }
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(StudentServlet.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(StudentServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
-
-    
 
 }
