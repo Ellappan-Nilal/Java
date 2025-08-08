b7625421a5bae986ddb69debbdbb78ba9baf03c8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-08 22:53:41 +0530

    File is change

--- Pattern/Star/Odd_Triangle_pattern.java
+++ Pattern/Star/Odd_Triangle_pattern.java
@@ -20,7 +20,7 @@ public class Odd_Triangle_pattern {
             for(int k=1;k<=2*i-1;k++){
                 System.out.print("*");
             }
-            System.out.println();
+           
         }
     } 
 }
