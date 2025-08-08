6ba13a69d6a457a0e023b7f100c2c676468e0d84
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-08 22:55:32 +0530

    File is change

--- Pattern/Star/Odd_Triangle_pattern.java
+++ Pattern/Star/Odd_Triangle_pattern.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
  */
 public class Odd_Triangle_pattern {
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
+        
         int n=sc.nextInt();
         for(int i=1;i<=n;i++){
             for(int j=1;j<=n-i;j++){
