755ec3a6bc78ce77b74105be176d3860e81a1c18
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-09 19:25:26 +0530

    File is change

--- Pattern/Character/Even_character.java
+++ Pattern/Character/Even_character.java
@@ -22,7 +22,7 @@ public static void main(String[] args){
                 System.out.print(c+" ");
                 c+=2;
             }
-            
+            System.out.println();
         }
     }    
 }
