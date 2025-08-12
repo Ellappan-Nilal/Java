bf59647fab7069be7c677d9e66c12d77628d620f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-12 22:42:48 +0530

    File is change

--- Leetcode/Merge_sorted_array.java
+++ Leetcode/Merge_sorted_array.java
@@ -4,10 +4,6 @@
  */
 package solve_problems.Leetcode;
 
-/**
- *
- * @author Nilal
- */
 public class Merge_sorted_array {
     public static void main(String[] args){
         int nums1[]={1,2,3,0,0,0};
