d9f12c26c444428bb92b68d9a40701ac39e64bf6
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-08-30 17:28:10 +0530

    file is change

--- Number_Series/Two_digit_number_print.java
+++ Number_Series/Two_digit_number_print.java
@@ -3,10 +3,6 @@ package solve_problems.Number_Series;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Nilal
- */
 public class Two_digit_number_print {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
