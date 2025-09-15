6077af808214307f83a02b35a037ac997f55cf99
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-15 22:12:41 +0530

    File is change

--- Leetcode/String_programming/Pascal_Triangle.java
+++ Leetcode/String_programming/Pascal_Triangle.java
@@ -3,7 +3,11 @@ package solve_problems.Leetcode.String_programming;
  import java.util.*;
 
 public class Pascal_Triangle {
-  
+  /*
+   * 
+   * Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+   */
 
 class Solution {
     public List<List<Integer>> generate(int numRows) {
