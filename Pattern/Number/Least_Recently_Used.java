06d796d5a57255af36f958b38fd19cfebffe69c3
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-16 21:08:16 +0530

    File is change

---
+++ Pattern/Number/Least_Recently_Used.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Pattern.Number;
+
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+/**
+ *
+ * @author Nilal
+ */
+public class Least_Recently_Used {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cache capacity: ");
+        int S = scanner.nextInt();
+        System.out.print("Enter number of pages: ");
+        int N = scanner.nextInt();
+        int[] pages = new int[N];
+
+        System.out.println("Enter page sequence:");
+        for (int i = 0; i < N; i++) {
+            pages[i] = scanner.nextInt();
+        }
+
+        Set<Integer> cache = new LinkedHashSet<>();
+        int missCount = 0;
+
+        for (int page : pages) {
+            if (!cache.contains(page)) {
+                missCount++;
+                if (cache.size() == S) {
+                    Iterator<Integer> iterator = cache.iterator();
+                    cache.remove(iterator.next());
+                }
+                cache.add(page);
+            } else {
+                cache.remove(page);
+                cache.add(page); // Update LRU order
+            }
+        }
+        System.out.println("Miss count: " + missCount);
+    }
+}
+
