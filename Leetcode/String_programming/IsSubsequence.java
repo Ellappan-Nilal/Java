cdbb72663f06012589d501a36c268d12db7e79f8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-17 22:19:56 +0530

    File code is added

--- Leetcode/String_programming/IsSubsequence.java
+++ Leetcode/String_programming/IsSubsequence.java
@@ -10,5 +10,29 @@ package solve_problems.Leetcode.String_programming;
  * @author Nilal
  */
 public class IsSubsequence {
-    
+    public static void main(String[] args){
+        String s="abc";
+        String t="ahbgdc";
+        System.out.print(IsSubsequence(s,t));
+    }
+    public static boolean IsSubsequence(String s,String t){
+        if(s.length()==0){
+            return true;
+        }
+        int i=0;
+        int j=0;
+        while(i<s.length()&&j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+                j++;
+            }
+            else{
+                j++;
+            }
+        }
+        if(i==s.length()){
+            return true;
+        }
+        return false;
+    }
 }
