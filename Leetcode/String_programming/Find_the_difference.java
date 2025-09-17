4f983ffcb9ef880bf5922e58abbe4e637886975a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-17 22:00:32 +0530

    code is added

--- Leetcode/String_programming/Find_the_difference.java
+++ Leetcode/String_programming/Find_the_difference.java
@@ -8,5 +8,19 @@ Output: "e"
 Explanation: 'e' is the letter that was added.
  */
 public class Find_the_difference {
-    
+    public static void main(String[] args){
+        String s="abce";
+        String t="abcde";
+        System.out.print(finddiffer(s,t));
+    }
+    public static char finddiffer(String s,String t){
+        int total=0;
+        for(int i=0;i<t.length();i++){
+            total=total+t.charAt(i);
+        }
+        for(int i=0;i<s.length();i++){
+            total=total-s.charAt(i);
+        }
+        return (char)total;
+    }
 }
