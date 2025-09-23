8c4dddad56473d1d6d7ebe4e08f22203312dfab2
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-23 11:16:42 +0530

    File is change

--- Leetcode/Index_of_First_Occurrence_String.java
+++ Leetcode/Index_of_First_Occurrence_String.java
@@ -9,18 +9,19 @@ package solve_problems.Leetcode;
  * @author Nilal
  */
 public class Index_of_First_Occurrence_String {
-    public static void main(String[] args){
-        System.out.print(StrStr(haystack: "sadbutsad",needle:"sad"));
+    public static void main(String[] args) {
+        System.out.println(strStr("sadbutsad", "sad"));
     }
-    public int strStr(String haystack, String needle) {
+
+    public static int strStr(String haystack, String needle) {
         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
             if (haystack.charAt(i) == needle.charAt(0)) {
                 if (haystack.substring(i, needle.length() + i).equals(needle)) {
                     return i;
                 }
             }
-
         }
         return -1;
     }
 }
+
--- Leetcode/String_programming/length_of_last_word.java
+++ Leetcode/String_programming/length_of_last_word.java
@@ -2,5 +2,20 @@
 package solve_problems.Leetcode.String_programming;
 
 public class length_of_last_word {
-    
+    public static void main(String[] args){
+        String s="fly of the moon";
+        System.out.println(Lengthoflast(s));
+    }
+    static int Lengthoflast(String s){
+        String str=s.trim();
+        int count=0;
+        for(int i=str.length()-1;i>=0;i--){
+            if(str.charAt(i)!=' '){
+                count++;
+            }else{
+                break;
+            }
+        }
+        return count;
+    }
 }
