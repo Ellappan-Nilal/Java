2db1a6ed1e66791802efabfe5a2e9cde788d27a8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-25 23:53:27 +0530

    File is change

--- Leetcode/Rotate_array.java
+++ Leetcode/Rotate_array.java
@@ -4,26 +4,34 @@ public class Rotate_array {
     public static void main(String[] args){
         int nums[]={1,2,3,4,5,6};
         int k=3;
-        System.out.println(Arrays);
+        System.out.println(Arrays.toString(rotate(nums,k)));
     }
-    public static int[] rotate(int[] nums,int k){
-        int n=nums.length;
-        reverse(nums,0,n-1);
-        reverse(nums,0,k-1);
-        reverse(nums,k,n-1);
-        return nums;
-    }
-    public static void reverse(int[] nums,int start,int end){
-        while(start<=end){
-            int temp=nums[start];
-            nums[start]=nums[end];
-            nums[end]=temp;
-            start++;
-            end--;
+    
+	public static void reverse(int nums[], int i, int j){
+        int li = i;
+        int ri = j;
+        
+        while(li < ri){
+            int temp = nums[li];
+            nums[li] = nums[ri];
+            nums[ri] = temp;
+            
+            li++;
+            ri--;
         }
     }
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length; 
+        if(k < 0){ 
+            k += nums.length;
+        }
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        reverse(nums, 0, nums.length - 1);
+    }
+}
 
 
 
     
-}
+
