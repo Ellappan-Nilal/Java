de6f56e19030292d145081263406ba732e01775f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-26 21:41:29 +0530

    File is change

--- Leetcode/Reverse_words_in_a_string.java
+++ Leetcode/Reverse_words_in_a_string.java
@@ -1,15 +1,17 @@
 public class Reverse_words_in_a_string{
   public static void main(String[] args){
-    System.out.println(reverWords("the sky is blue"));
+    System.out.println(reverseWords("the sky is blue"));
   }
-}
+
 
 
 static String reverseWords(String s){
   String[] words=s.split(" +");
   StringBuilder sb=new StringBuilder();
   for(int i=words.length-1;i>=0;i--){
+      sb.append(words[i]);
       sb.append(" ");
   }
   return sb.toString().trim();
+}
 }
