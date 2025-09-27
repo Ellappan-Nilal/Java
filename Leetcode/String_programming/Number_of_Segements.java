2706b40cce2c0d705e12c869bd06f0d6fe23764f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-27 22:11:40 +0530

    File is change

--- Leetcode/String_programming/Number_of_Segements.java
+++ Leetcode/String_programming/Number_of_Segements.java
@@ -2,5 +2,22 @@
 package solve_problems.Leetcode.String_programming;
 
 public class Number_of_Segements {
-    
+    public static void main(String[] s){
+       String text="Hello, my dear sister";
+       System.out.print(segements(text));
+    }
+    public static int segements(String seg){
+        int c=0;
+        if(seg.equals("")){
+            return 0;
+        }
+        String[] a=seg.split(" ");
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=""){
+                c++;
+            }
+        }
+            
+        return c;
+    }
 }
