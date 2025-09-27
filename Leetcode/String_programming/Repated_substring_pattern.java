0374a0c89e441e2dd7f08304c955cc107bfc0c78
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-27 22:13:43 +0530

    File is adedd

---
+++ Leetcode/String_programming/Repated_substring_pattern.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.Leetcode.String_programming;
+
+/**
+ *
+ * Input: s = "abab"
+Output: true
+Explanation: It is the substring "ab" twice.
+Example 2:
+
+Input: s = "aba"
+Output: false
+ */
+public class Repated_substring_pattern {
+    
+}
