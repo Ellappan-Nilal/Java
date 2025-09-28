37cffa2d3002d50001df9e15577de0564c57966d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-28 21:47:53 +0530

    File is change

--- Leetcode/String_programming/longest_uncommon_subsequence.java
+++ Leetcode/String_programming/longest_uncommon_subsequence.java
@@ -1,5 +1,21 @@
 package solve_problems.Leetcode.String_programming;
 
 public class longest_uncommon_subsequence {
-    
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return a.length() > b.length() ? a.length() : b.length();
+    }
+
+    public static void main(String[] args) {
+        longest_uncommon_subsequence sol = new longest_uncommon_subsequence();
+
+        String a = "aba";
+        String b = "cdc";
+
+        int result = sol.findLUSlength(a, b);
+        System.out.println("Input: a = \"" + a + "\", b = \"" + b + "\"");
+        System.out.println("Output: " + result); // Expected: 3
+    }
 }
