81ae3bff71ac28b29acde4ef2fce249ac4db0ca4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-28 21:37:36 +0530

    File is change

--- Leetcode/String_programming/Captial_word_find.java
+++ Leetcode/String_programming/Captial_word_find.java
@@ -1,6 +1,18 @@
-
 package solve_problems.Leetcode.String_programming;
 
+import java.util.regex.Pattern;
+
 public class Captial_word_find {
-    
+    public boolean detectCapitalUse(String word) {
+        return Pattern.matches("([A-Z]+|[a-z]+|[A-Z][a-z]*)", word);
+    }
+
+    public static void main(String[] args) {
+        Captial_word_find sol = new Captial_word_find();
+
+        System.out.println(sol.detectCapitalUse("USA"));      // true
+        System.out.println(sol.detectCapitalUse("leetcode")); // true
+        System.out.println(sol.detectCapitalUse("Google"));   // true
+        System.out.println(sol.detectCapitalUse("FlaG"));     // false
+    }
 }
