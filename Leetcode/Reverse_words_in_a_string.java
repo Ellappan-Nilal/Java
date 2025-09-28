be19dc3871568fabec0c2ab9c86611c35d7ff9d5
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-09-28 21:50:28 +0530

    File is change

--- Leetcode/Reverse_words_in_a_string.java
+++ Leetcode/Reverse_words_in_a_string.java
@@ -1,17 +1,20 @@
-public class Reverse_words_in_a_string{
-  public static void main(String[] args){
-    System.out.println(reverseWords(""));
-  }
+public class Reverse_words_in_a_string {
+    public static void main(String[] args) {
+        
+        System.out.println(reverseWords("the sky is blue"));          // "blue is sky the"
+        System.out.println(reverseWords("  hello world  "));          // "world hello"
+        System.out.println(reverseWords("a good   example"));         // "example good a"
+        System.out.println(reverseWords(""));                         // ""
+        System.out.println(reverseWords("single"));                   // "single"
+    }
 
-
-
-static String reverseWords(String s){
-  String[] words=s.split(" +");
-  StringBuilder sb=new StringBuilder();
-  for(int i=words.length-1;i>=0;i--){
-      sb.append(words[i]);
-      sb.append(" ");
-  }
-  return sb.toString().trim();
-}
+    static String reverseWords(String s) {
+        String[] words = s.trim().split(" +");
+        StringBuilder sb = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i > 0) sb.append(" ");
+        }
+        return sb.toString();
+    }
 }
