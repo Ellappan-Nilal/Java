80b7363b5d3d82db966e0d0e51355da2b80e0e38
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-09 22:40:30 +0530

    File is chnage

--- Leetcode/String_programming/MostCommonCharacters.java
+++ Leetcode/String_programming/MostCommonCharacters.java
@@ -30,7 +30,7 @@ public class MostCommonCharacters {
     public static void main(String[] args) {
         MostCommonCharacters sol = new MostCommonCharacters();
 
-        // Example 1
+        
         String[] words1 = {"bella", "label", "roller"};
         List<String> result1 = sol.commonChars(words1);
         System.out.println("Example 1 Output: " + result1);
