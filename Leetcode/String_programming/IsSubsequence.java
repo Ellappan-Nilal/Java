1a73adb0a88fb24eecc2297325639fbb92f40468
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-13 21:58:06 +0530

    File is change

--- Leetcode/String_programming/IsSubsequence.java
+++ Leetcode/String_programming/IsSubsequence.java
@@ -1,10 +1,5 @@
 package solve_problems.Leetcode.String_programming;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 
 public class IsSubsequence {
     public static void main(String[] args){
