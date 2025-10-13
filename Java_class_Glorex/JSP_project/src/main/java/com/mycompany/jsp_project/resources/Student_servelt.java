02631f55771f18f041279d35cc676bf0f7540eef
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-13 21:52:57 +0530

    File is change

--- Java_class_Glorex/JSP_project/src/main/java/com/mycompany/jsp_project/resources/Student_servelt.java
+++ Java_class_Glorex/JSP_project/src/main/java/com/mycompany/jsp_project/resources/Student_servelt.java
@@ -1,5 +1,10 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,6 +24,14 @@ public class Student_servelt extends HttpServlet {
         String studentMobile = request.getParameter("Smobile");
         
         PrintWriter out=response.getWriter();
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/learn", "root", "");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Student_servelt.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Student_servelt.class.getName()).log(Level.SEVERE, null, ex);
+        }
         
        out.println("Student Info:");
        out.println("Name: " + studentName);
