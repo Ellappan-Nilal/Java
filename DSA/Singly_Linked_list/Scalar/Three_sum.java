340777b92e1070332d1a1b518c2bff60f5805b3c
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-15 22:42:49 +0530

    Code is added

--- DSA/Singly_Linked_list/Scalar/Three_sum.java
+++ DSA/Singly_Linked_list/Scalar/Three_sum.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.DSA.Singly_Linked_list.Scalar;
 
-/**
- *
- * @author Nilal
- */
+import java.util.Arrays;
+
 public class Three_sum {
-    
+    public static void main(String[] args){
+        int[] arr={7,-6,3,8,-1,8,-11};
+        int target=0;
+        sollution(arr,target,arr.length);
+    }
+    public static void sollution(int[] a,int target,int n){
+        Arrays.sort(a);
+        for(int i=0;i<n;i++){
+            if(i==0 || (a[i]!=a[i-1])){
+                int j=i+1,k=n-1;
+                int tar=target-a[i];
+                while(j<k){
+                    if(a[j]+a[k]==tar){
+                        System.out.print(a[i]+" "+a[j]+" "+a[k]);
+                        while(j<k && a[j]==a[j+1])j++;
+                        while(j<k && a[j]==a[k-1])k--;
+                            
+                        j++;
+                        k--;
+                    }else if (a[j]+a[k]<tar){
+                        j++;
+                    }else{
+                        k--;
+                    }
+                }
+            }
+        }
+    }
 }
