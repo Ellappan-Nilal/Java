6b31ae066e7cc26429fee64199f46a072cb65234
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-17 09:40:21 +0530

    File is change

--- DSA/Singly_Linked_list/Scalar/Majority_Element.java
+++ DSA/Singly_Linked_list/Scalar/Majority_Element.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.DSA.Singly_Linked_list.Scalar;
 
-/**
- *
- * @author Nilal
- */
 public class Majority_Element {
-    
+    public static void main(String[] args){
+            int[] arr={5,7,4,7,4,4,5,4,4,7,4,4};
+            System.out.println(findmajorityelement(arr,arr.length));
+    }
+    public static int findmajorityelement(int[] arr,int n){
+        int candidate=arr[0];
+        int count=1;
+         for(int i=1;i<n;i++){
+             if(arr[i]==candidate){
+                 count++;
+             }else{
+                 count--;
+             }
+             if(count==0){
+                 candidate=arr[i];
+                 count=1;
+             }
+         }//verify if candidate is yor asnor not 
+         count=0;
+         for(int val:arr){
+             if(val==candidate){
+                 count++;
+             }             
+         }
+         if(count>n/2){
+             return candidate;
+         }else{
+             return -1;
+         }
+         
+    }
 }
