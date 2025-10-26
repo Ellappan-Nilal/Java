84b17cc6f8374df3eae5a470c960de7de4971477
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-26 22:00:30 +0530

    File is cahnge

--- DSA/Singly_Linked_list/Scalar/Matrix_90_degree.java
+++ DSA/Singly_Linked_list/Scalar/Matrix_90_degree.java
@@ -3,4 +3,33 @@ package solve_problems.DSA.Singly_Linked_list.Scalar;
 
 public class Matrix_90_degree {
     
+    public static void main(String[] args){
+        int[][] arr={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+        rotate90(arr);
+        print(arr);
+    }
+    public static void print(int[][] arr){
+        
+    }
+    public static void rotate90(int[][] arr){
+        //transpose
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr[0].length;j++){
+                int temp=arr[i][j];
+                arr[i][j]=arr[j][i];
+                arr[j][i]=temp;
+            }
+        }
+        //swap column
+        int left=0, right=arr[0].length-1;
+        while(left<right){
+            for(int i=0;i<arr.length;i++){
+                int temp=arr[i][left];
+                arr[i][left]=arr[i][right];
+                arr[i][right]=temp;
+            }
+            left++;
+            right--;
+        }
+    }
 }
