b20b01b9060f29276e203a3afe31bb2c7b3c91ae
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-29 21:49:24 +0530

    File is change

--- DSA/Scalar/First_and_Last_index_X.java
+++ DSA/Scalar/First_and_Last_index_X.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.DSA.Scalar;
 public class First_and_Last_index_X {
     
