7b8f64619e2135754a0c57ff32b85b94ee9d891d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-29 22:25:04 +0530

    File is read

--- DSA/Scalar/First_and_Last_index_X.java
+++ DSA/Scalar/First_and_Last_index_X.java
@@ -1,5 +1,45 @@
-
 package solve_problems.DSA.Scalar;
+
 public class First_and_Last_index_X {
-    
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 7, 7,8, 9};
+        postion(arr, 7);
+    }
+
+    public static void postion(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+        int first = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                first=mid;
+                right=mid-1;
+            }else if(arr[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            
+        }
+         left = 0;
+        right = arr.length - 1;
+        int last = -1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] == target) {
+                last=mid;
+                left=mid+1;
+            }else if(arr[mid]<target){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+            
+        }
+        System.out.println(first+" "+last);
+    }
 }
+
+
