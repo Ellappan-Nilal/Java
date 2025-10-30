c7d81be547d3a837780e7da36df9b1e123dafda7
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-10-30 22:03:02 +0530

    File is change

---
+++ DSA/Scalar/Binary_search/Search_in_sorted_and_rotated_array.java
@@ -0,0 +1,10 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.DSA.Scalar.Binary_search;
+
+
+public class Search_in_sorted_and_rotated_array {
+    
+}
