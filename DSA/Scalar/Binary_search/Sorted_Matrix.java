0f52a9b1293d9fe72ece736bb7e803cbbbe8dc70
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-01 21:23:02 +0530

    File is create

---
+++ DSA/Scalar/Binary_search/Sorted_Matrix.java
@@ -0,0 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solve_problems.DSA.Scalar.Binary_search;
+
+public class Sorted_Matrix {
+    
+}
