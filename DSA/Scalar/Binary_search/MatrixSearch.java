2ab303db6e54b304f8307cc2d558d70ad8bf35fb
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-02 21:57:06 +0530

    File is change

---
+++ DSA/Scalar/Binary_search/MatrixSearch.java
@@ -0,0 +1,37 @@
+package DSA.Scalar.Binary_search;
+
+public class MatrixSearch {
+    public static int searchMatrix(int[][] A, int B) {
+        int n = A.length;
+        int m = A[0].length;
+
+        int left = 0, right = n * m - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            int row = mid / m;
+            int col = mid % m;
+
+            if (A[row][col] == B) return 1;
+            else if (A[row][col] < B) left = mid + 1;
+            else right = mid - 1;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int[][] A1 = {
+            {1, 3, 5, 7},
+            {10, 11, 16, 20},
+            {23, 30, 34, 50}
+        };
+        System.out.println(searchMatrix(A1, 3)); // Output: 1
+
+        int[][] A2 = {
+            {5, 17, 100, 111},
+            {119, 120, 127, 131}
+        };
+        System.out.println(searchMatrix(A2, 3)); // Output: 0
+    }
+}
