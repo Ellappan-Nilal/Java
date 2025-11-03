13b373892955a8308c4446233ac446b16237762b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-03 22:14:32 +0530

    File is added

--- DSA/Scalar/TargetSumSubset.java
+++ DSA/Scalar/TargetSumSubset.java
@@ -1,5 +1,14 @@
 package solve_problems.DSA.Scalar;
 
+import java.util.ArrayList;
+
 public class TargetSumSubset {
-    
+    public static void main(String[] args){
+        
+    }
+    public static void tes(int[] arr, int pos, boolean[] selected, ArrayList<Integer> ans){
+    //select ith element
+    ans.add[arr[i]];
+    tes(arr,i+1,length-arr[i],ans);
+    }
 }
