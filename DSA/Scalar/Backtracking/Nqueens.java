09c3a23cd09b32679c7b5666c3516357c35af4aa
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-13 22:59:26 +0530

    File code is change

--- DSA/Scalar/Backtracking/Nqueens.java
+++ DSA/Scalar/Backtracking/Nqueens.java
@@ -1,11 +1,11 @@
-
-package solve_problems.DSA.Scalar.Backtracking;
-
-public class Nqueens {
-    public static void main(String[] args){
-        
-    }
-    public static void nqueens(char[][] arr,int row ){
-        
-    }
+import java.util.*;
+public class Main{
+public static void main(String[] args) { -
+}
+public static void print Permutations (int[] arr, int pos, boolean[] selected, ArrayList<Integer> ans) { -
+}
+public static void tss (int () arr, int i, int target, ArrayList<Integer> ans){-
+}
+public static void nQueens (char[] [] arr, int row
+C 10 10
 }
