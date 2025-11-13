dd2b008b30b4fb0a0151fa96a947481ed953c041
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-13 23:10:38 +0530

    File is chnage

--- DSA/Scalar/Backtracking/Nqueens.java
+++ DSA/Scalar/Backtracking/Nqueens.java
@@ -5,7 +5,7 @@ public static void main(String[] args) {
     char[][] arr=new char[n][n];
     for(int i=0;i<arr.length;i++){
         for(int j=0;j<arr.length;j++){
-            
+            arr[i][j]='.';
         }
     }
 }
