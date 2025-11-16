401838d6225e79b9115416b2b657a870b4f495e2
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-16 21:29:25 +0530

    File is change

--- DSA/Scalar/Backtracking/Nqueens.java
+++ DSA/Scalar/Backtracking/Nqueens.java
@@ -14,8 +14,21 @@ public static void print Permutations (int[] arr, int pos, boolean[] selected, A
 }
 public static void tss (int () arr, int i, int target, ArrayList<Integer> ans){-
 }
+public static void print(char[][] arr){
+    for(int i=0;i<arr.length;i++){
+        for(int j=0;j<arr[0].length;j++){
+            System.out.println(arr[i][j]+" ");
+        }
+        System.out.println();
+    }
+}
 public static void nQueens (char[] [] arr, int row){
-   for(int col=0;col<arr[0].length;col++) {
+   if(row==arr.length){
+       //decide for every row
+       print(arr);
+       return ;
+   }
+    for(int col=0;col<arr[0].length;col++) {
        if(isSafe(arr,row,col)==true){
            arr[row][col]='Q';
            nQueens(arr,row+1);
