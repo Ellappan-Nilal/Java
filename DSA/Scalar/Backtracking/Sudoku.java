7ae2e7633189df737824e30f731bfe9059bc6e86
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-17 22:03:43 +0530

    File is chnage

--- DSA/Scalar/Backtracking/Sudoku.java
+++ DSA/Scalar/Backtracking/Sudoku.java
@@ -2,5 +2,17 @@
 package solve_problems.DSA.Scalar.Backtracking;
 
 public class Sudoku {
-    
+    public static void main(String[] args){
+        
+    }
+    public static void solvesudoku(char[] arr){
+        if(sudokusolve(arr,0,0)){
+            for(int i=0;i<arr.length;i++){
+                for(int j=0;j<arr[0].length;j++){
+                    System.out.print(arr[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
 }
