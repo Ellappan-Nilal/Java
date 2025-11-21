70d204c0e83a9566104710b332da8c2c6564c33f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-21 11:25:15 +0530

    File is chnage

--- DSA/Scalar/Majority_Element.java
+++ DSA/Scalar/Majority_Element.java
@@ -19,6 +19,9 @@ public class Majority_Element {
                  count=1;
              }
          }//verify if candidate is yor asnor not 
+
+
+         
          count=0;
          for(int val:arr){
              if(val==candidate){
