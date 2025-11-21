058c2f063b3b14bebe8e3b3b77ce4cb415966679
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-11-21 11:14:32 +0530

    FIle is change

--- DSA/Scalar/Backtracking/Sudoku.java
+++ DSA/Scalar/Backtracking/Sudoku.java
@@ -52,6 +52,7 @@ public class Sudoku {
         return false;
     }
     
+    
     public static boolean canWePlace(char[][] arr, int x, int y, int val) {
         char ch = (char) (val + '0');
         // Check row
