59ff8608c4434c314c338288f6cae9703c7361bf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-01 21:59:29 +0530

    File is change

--- Leetcode/String_programming/ExpressionMatching.java
+++ Leetcode/String_programming/ExpressionMatching.java
@@ -1,40 +1,47 @@
-
 public class ExpressionMatching {
 
-    public class Solution {
+    public static class Solution {
 
         public boolean isMatch(String s, String p) {
+
+            // If pattern is empty, string must also be empty
             if (p.length() == 0) {
                 return s.length() == 0;
             }
-//p’s length 1 is special case
-            if (p.length() == 1 || p.charAt(1) !=  {
-                
-            }
-            
-                ’*’){
-if (s.length() < 1 || (p.charAt(0) !=  {
-                    
-                }’.’ && s.charAt(0)
-                        != p.charAt(0)
-                ))
-return false;
+
+            // Case 1: Pattern length is 1 OR next character is not '*'
+            if (p.length() == 1 || p.charAt(1) != '*') {
+                if (s.length() < 1 || (p.charAt(0) != '.' && s.charAt(0) != p.charAt(0))) {
+                    return false;
+                }
                 return isMatch(s.substring(1), p.substring(1));
-            }else{
-                        int len = s.length();
-                        int i = -1;
-                        while (i < len && (i < 0 || p.charAt(0) ==  {
-                            
-                        }’.’ || p.charAt(0)
-                        == s.charAt(i)
-                            )){
-if (isMatch(s.substring(i + 1), p.substring(2))) {
-                                return true;
-                            }
-                            i++;
-                        }
-return false;
-}
+            }
+
+            // Case 2: Next char is '*'
+            else {
+                int len = s.length();
+                int i = -1;
+
+                // Try all possible prefixes
+                while (i < len && (i < 0 || p.charAt(0) == '.' || p.charAt(0) == s.charAt(i))) {
+
+                    // Skip the x* part
+                    if (isMatch(s.substring(i + 1), p.substring(2))) {
+                        return true;
+                    }
+                    i++;
+                }
+                return false;
+            }
         }
+    }
+
+    // Quick test
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.isMatch("aa", "a*"));       // true
+        System.out.println(sol.isMatch("ab", ".*"));       // true
+        System.out.println(sol.isMatch("aab", "c*a*b"));   // true
+        System.out.println(sol.isMatch("mississippi", "mis*is*p*.")); // false
     }
 }
