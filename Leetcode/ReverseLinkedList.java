d143205e831850e99b0d99a1990ad7cf5a9203f5
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-04 19:42:41 +0530

    File code is added

--- Leetcode/ReverseLinkedList.java
+++ Leetcode/ReverseLinkedList.java
@@ -1,32 +1,58 @@
-
 package solve_problems.Leetcode;
-public class ListNode{
+
+
+class ListNode {
     int val;
     ListNode next;
-    ListNode(){}
-    ListNode(int val){this.val=val;}
-    ListNode(int val,ListNode next){this.val=val;this.next=next;}
-}
-public class ReverseLinkedList {
-    public static void main(String[] args){
-        
+
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
     }
-    public ListNode reverselist(ListNode head){
-        if(head==null){
-            return head;
+}
+
+public class ReverseLinkedList {
+
+    public static void main(String[] args) {
+
+        
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        ReverseLinkedList obj = new ReverseLinkedList();
+        ListNode reversed = obj.reverseList(head);
+
+   
+        System.out.print("Reversed List: ");
+        printList(reversed);
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next; 
+            current.next = prev;              
+            prev = current;                   
+            current = nextNode;              
         }
-        ListNode previous=null;
-        ListNode current=head;
-        ListNode next=current.next;
-        while(current!=null){
-            current.next=previous;
-            previous=current;
-            current=next;
-            if(next!=null){
-                next=current.next;
-            }
+
+        return prev; 
+    }
+
+    
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
         }
-        head=previous;
-        return head;
+        System.out.println();
     }
 }
