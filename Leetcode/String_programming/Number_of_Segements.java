de7d50b90644a8bae492826e52b4d0f18051bee4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-05 18:11:56 +0530

    File is chnage

--- Leetcode/String_programming/Number_of_Segements.java
+++ Leetcode/String_programming/Number_of_Segements.java
@@ -11,6 +11,8 @@ public class Number_of_Segements {
         if(seg.equals("")){
             return 0;
         }
+
+        
         String[] a=seg.split(" ");
         for(int i=0;i<a.length;i++){
             if(a[i]!=""){
