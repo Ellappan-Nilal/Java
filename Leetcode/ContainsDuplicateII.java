4e41e6336ee54fc45bf187bfbed8b1a75fa2a84b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-06 21:58:02 +0530

    File code is added

--- Leetcode/ContainsDuplicateII.java
+++ Leetcode/ContainsDuplicateII.java
@@ -1,9 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.Leetcode;
 
 public class ContainsDuplicateII {
-    
+    public static void main(String[] args){
+        
+    }
+    public boolean containsNearByDuplicate(int[] nums,int k){
+        Map<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i],i);
+            }
+            else{
+                int diff=Math.abs(map.get(nums[i])-i);
+                if(diff<=k){
+                    return true;
+                }
+                else{
+                    map.put(nums[i],i);
+                }
+            }
+        }
+        return false;
+    }
 }
