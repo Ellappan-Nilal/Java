e905b685d398bcd3d991cfafdb870e4547a85316
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-06 21:59:24 +0530

    File code is modified

--- Leetcode/ContainsDuplicateII.java
+++ Leetcode/ContainsDuplicateII.java
@@ -1,23 +1,32 @@
-
 package solve_problems.Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ContainsDuplicateII {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        ContainsDuplicateII obj = new ContainsDuplicateII();
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+
+        boolean result = obj.containsNearByDuplicate(nums, k);
+        System.out.println(result); // true
     }
-    public boolean containsNearByDuplicate(int[] nums,int k){
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i],i);
-            }
-            else{
-                int diff=Math.abs(map.get(nums[i])-i);
-                if(diff<=k){
+
+    public boolean containsNearByDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (!map.containsKey(nums[i])) {
+                map.put(nums[i], i);
+            } else {
+                int diff = Math.abs(map.get(nums[i]) - i);
+                if (diff <= k) {
                     return true;
-                }
-                else{
-                    map.put(nums[i],i);
+                } else {
+                    map.put(nums[i], i);
                 }
             }
         }
