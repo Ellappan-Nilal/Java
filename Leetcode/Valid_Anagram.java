e9ae14e1e4c887df5e5076d2b30e67661e4b0081
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-07 22:23:02 +0530

    File is modify

--- Leetcode/Valid_Anagram.java
+++ Leetcode/Valid_Anagram.java
@@ -1,28 +1,47 @@
-
 package solve_problems.Leetcode;
 
 public class Valid_Anagram {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+
+        Valid_Anagram obj = new Valid_Anagram();
+
+        // Example 1
+        String s1 = "anagram";
+        String t1 = "nagaram";
+        System.out.println(obj.isAnagram(s1, t1)); // true
+
+        // Example 2
+        String s2 = "rat";
+        String t2 = "car";
+        System.out.println(obj.isAnagram(s2, t2)); // false
     }
-    public boolean isAnagram(String s,String t){
-        if(s.length()!=t.length()){
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
             return false;
         }
-        int count[] =new int[26];
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            count[ch-'a']++;
+
+        int[] count = new int[26];
+
+       
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
         }
-        for(int i=0;i<t.length();i++){
-            char ch=t.charAt(i);
-            count[ch-'a']--;
+
+        
+        for (int i = 0; i < t.length(); i++) {
+            count[t.charAt(i) - 'a']--;
         }
-        for(int i=0;i<count.length;i++){
-            if(count[i]>0){
+
+        
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
