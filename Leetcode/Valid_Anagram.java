cfa86c737084c15571d5ef9324258f27bac08b8d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-07 22:20:24 +0530

    File is code is added

--- Leetcode/Valid_Anagram.java
+++ Leetcode/Valid_Anagram.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.Leetcode;
 
-/**
- *
- * @author Nilal
- */
 public class Valid_Anagram {
-    
+    public static void main(String[] args){
+        
+    }
+    public boolean isAnagram(String s,String t){
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int count[] =new int[26];
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            count[ch-'a']++;
+        }
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            count[ch-'a']--;
+        }
+        for(int i=0;i<count.length;i++){
+            if(count[i]>0){
+                return false;
+            }
+        }
+        return true;
+    }
 }
