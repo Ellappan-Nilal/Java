d9525adf0671f09a407c81757b6ebfe3b1ce8505
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-07 22:43:26 +0530

    FIle is change

--- Leetcode/MoveZeros.java
+++ Leetcode/MoveZeros.java
@@ -1,13 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solve_problems.Leetcode;
 
-/**
- *
- * @author Nilal
- */
+import java.util.Arrays;
+
 public class MoveZeros {
-    
+
+    public static void main(String[] args) {
+
+        MoveZeros obj = new MoveZeros();
+
+        
+        int[] nums = {0, 1, 0, 3, 12};
+
+        obj.moveZeroes(nums);
+
+        
+        System.out.println(Arrays.toString(nums)); // [1, 3, 12, 0, 0]
+    }
+
+    public void moveZeroes(int[] nums) {
+        int index = 0;
+
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        
+        for (int i = index; i < nums.length; i++) {
+            nums[i] = 0;
+        }
+    }
 }
