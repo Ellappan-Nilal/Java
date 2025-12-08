6462066e0cb70caf0d6dc8f14be5e79358cd71ee
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-08 21:36:58 +0530

    File code is added

--- Leetcode/ReverseString.java
+++ Leetcode/ReverseString.java
@@ -2,5 +2,19 @@
 package solve_problems.Leetcode;
 
 public class ReverseString {
-    
+    public static void main(String[] args){
+        
+    }
+    public void reverseString(char[] s){
+        int start=0;
+        int end=s.length-1;
+        while(start<end){
+            char ch=s[start];
+            s[start]=s[end];
+            s[end]=ch;
+            start++;
+            end--;
+        }
+                
+    }
 }
