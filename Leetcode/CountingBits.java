3bd5835a50cce26de0f26e87714e424e37322ce1
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-08 21:12:23 +0530

    FIle code is added

--- Leetcode/CountingBits.java
+++ Leetcode/CountingBits.java
@@ -1,9 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package solve_problems.Leetcode;
 
 public class CountingBits {
-    
+    public static void main(String[] args){
+        
+    }
+    public int[] countBits(int n){
+        int ans[] =new int[n+1];
+        ans[0]=0;
+        for(int i=1;i<=n;i++){
+            String binary=Integer.toBinaryString(i);
+            int one_count=countOnes(binary);
+            int[i]=one_count;
+        }
+    }
+    private int countOnes(String num){
+        int count=0;
+        for(int i=0;i<num.length();i++){
+            int count=0;
+            for(int i=0;i<num.length();i++){
+                if(num.charAt(i)=='1'){
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
 }
