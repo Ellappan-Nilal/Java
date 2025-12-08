c60d20318c0612776e4c5ff202a3cf9f27d5943d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-08 22:13:27 +0530

    File code is added

--- Leetcode/Guess_no_higher_or_lower.java
+++ Leetcode/Guess_no_higher_or_lower.java
@@ -1,25 +1,47 @@
-
 package solve_problems.Leetcode;
 
 public class Guess_no_higher_or_lower {
-    public static void main(String[] args){
+
+    
+    static class GuessGame {
+        int pick = 6; 
+
         
+        int guess(int num) {
+            if (num == pick) return 0;
+            else if (num > pick) return -1;
+            else return 1;
+        }
     }
-    public class Solution extends guessgame{
-        public int guessno(int n){
-            int start=1;
-            int end=n;
-            while(start<=end){
-                int mid=start+(end-start)/2;
-                if(guess(mid)==0){
+
+    
+    static class Solution extends GuessGame {
+
+        public int guessNumber(int n) {
+            int start = 1;
+            int end = n;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                int res = guess(mid);
+
+                if (res == 0) {
                     return mid;
-                }else if(guess(mid)==1){
-                    start=mid+1;
-                }else{
-                    end=mid-1;
+                } else if (res == 1) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
                 }
             }
             return -1;
         }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 10;
+
+        int guessedNumber = solution.guessNumber(n);
+        System.out.println("Guessed Number: " + guessedNumber);
     }
 }
