dfd6aee8873fd2e0aea9ce97e281756a1ee554b2
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-08 21:17:42 +0530

    FIle is change

--- Leetcode/CountingBits.java
+++ Leetcode/CountingBits.java
@@ -1,29 +1,26 @@
-
 package solve_problems.Leetcode;
 
+import java.util.Arrays;
+
 public class CountingBits {
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
+        CountingBits solution = new CountingBits();
         
+        int n = 5;  
+        int[] result = solution.countBits(n);
+        
+        System.out.println(Arrays.toString(result));
+        // Output: [0, 1, 1, 2, 1, 2]
     }
-    public int[] countBits(int n){
-        int ans[] =new int[n+1];
-        ans[0]=0;
-        for(int i=1;i<=n;i++){
-            String binary=Integer.toBinaryString(i);
-            int one_count=countOnes(binary);
-            int[i]=one_count;
+
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            ans[i] = ans[i >> 1] + (i & 1);
         }
-    }
-    private int countOnes(String num){
-        int count=0;
-        for(int i=0;i<num.length();i++){
-            int count=0;
-            for(int i=0;i<num.length();i++){
-                if(num.charAt(i)=='1'){
-                    count++;
-                }
-            }
-            return count;
-        }
+
+        return ans;
     }
 }
