f54aee4ff374918bc84fef6cc092118cea1c2fd4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-08 21:39:39 +0530

    File code is modified

--- Leetcode/ReverseString.java
+++ Leetcode/ReverseString.java
@@ -1,20 +1,30 @@
-
 package solve_problems.Leetcode;
 
+import java.util.Arrays;
+
 public class ReverseString {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        ReverseString solution = new ReverseString();
+
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        solution.reverseString(s);
+
+        System.out.println(Arrays.toString(s));
+        // Output: [o, l, l, e, h]
     }
-    public void reverseString(char[] s){
-        int start=0;
-        int end=s.length-1;
-        while(start<end){
-            char ch=s[start];
-            s[start]=s[end];
-            s[end]=ch;
+
+    public void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length - 1;
+
+        while (start < end) {
+            char ch = s[start];
+            s[start] = s[end];
+            s[end] = ch;
             start++;
             end--;
         }
-                
     }
 }
