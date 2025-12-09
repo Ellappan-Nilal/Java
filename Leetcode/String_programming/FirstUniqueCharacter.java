75cac7a88ee09aef5c76016510bd56a762677ae9
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-09 22:20:23 +0530

    File coded is added

--- Leetcode/String_programming/FirstUniqueCharacter.java
+++ Leetcode/String_programming/FirstUniqueCharacter.java
@@ -2,5 +2,24 @@
 package solve_problems.Leetcode.String_programming;
 
 public class FirstUniqueCharacter {
-    
+    public static void main(String[] args){
+        
+    }
+    public int firstUniqChar(String s){
+        Map<Character,Integer> map=new HashMap<Character,Integer>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(map.containsKey(ch)){
+                map.put(ch,map.get(ch)+1);
+            }else{
+                map.put(ch,1);
+            }
+        }
+        for(int i=0;i<s.length();i++){
+             char ch=s.charAt(i);
+            if(map.get(ch)==1){
+                return i;
+            }
+        }
+    }
 }
