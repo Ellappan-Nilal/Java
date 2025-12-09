ab96db8cc74a597ba0031d74649f462a2b270aa4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-09 22:50:30 +0530

    File code is modify

--- Leetcode/String_programming/FindDifference.java
+++ Leetcode/String_programming/FindDifference.java
@@ -1,28 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solve_problems.Leetcode.String_programming;
 
-/**
- *
- * @author Nilal
- */
 public class FindDifference {
 
     public static void main(String[] args) {
 
+        FindDifference obj = new FindDifference();
+
+        String s = "abcd";
+        String t = "abcde";
+
+        char result = obj.findthediffernce(s, t);
+        System.out.println("The extra character is: " + result);
     }
 
     public char findthediffernce(String s, String t) {
         int s_sum = 0;
         int t_sum = 0;
+
         for (int i = 0; i < s.length(); i++) {
             s_sum += s.charAt(i);
         }
+
         for (int i = 0; i < t.length(); i++) {
             t_sum += t.charAt(i);
         }
+
         int result = t_sum - s_sum;
         return (char) result;
     }
