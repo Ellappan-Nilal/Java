87763d20696458f7bba3025c98b7e159a6c6f678
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-09 21:57:50 +0530

    File code is added

--- Leetcode/Ransomnote.java
+++ Leetcode/Ransomnote.java
@@ -1,33 +1,39 @@
-
 package solve_problems.Leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ransomnote {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        Ransomnote obj = new Ransomnote();
+
+        String ransomNote = "aa";
+        String magazine = "aab";
+
+        boolean result = obj.canConstruct(ransomNote, magazine);
+        System.out.println("Can construct ransom note: " + result);
     }
-    public boolean canConstruct(String ransomNote,String magazine){
-        Map<Character,Integer> map=new HashMap<>();
-        for(int i=0;i<magazine.length();i++){
-            if(map.containsKey(magazine.charAt(i))){
-                map.put(magazine.charAt(i),map.get(magazine.charAt(i))+1);
-            }else{
-                map.put(magazine.charAt(i),1);
+
+    public boolean canConstruct(String ransomNote, String magazine) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        // Count characters in magazine
+        for (int i = 0; i < magazine.length(); i++) {
+            char ch = magazine.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+
+        // Check characters in ransomNote
+        for (int i = 0; i < ransomNote.length(); i++) {
+            char ch = ransomNote.charAt(i);
+            if (map.containsKey(ch) && map.get(ch) > 0) {
+                map.put(ch, map.get(ch) - 1);
+            } else {
+                return false;
             }
         }
-        int count=0;
-        for(int i=0;i<ransomNote.length();i++){
-            char ch=ransomNote.charAt(i);
-            if(map.containsKey(ch)&&map.get(ch)>0){
-                count++;
-                map.put(ch,map.get(ch)-1);
-            }else{
-                break;
-            }
-        }
-        if(count==ransomNote.length()){
-            return true;
-        }else{
-            return false;
-        }
+
+        return true;
     }
 }
