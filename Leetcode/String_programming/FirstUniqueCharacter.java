98822735103815871b0a0f1587e98230d85e035b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-09 22:26:40 +0530

    Mainfunction code is added

--- Leetcode/String_programming/FirstUniqueCharacter.java
+++ Leetcode/String_programming/FirstUniqueCharacter.java
@@ -1,25 +1,35 @@
-
 package solve_problems.Leetcode.String_programming;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FirstUniqueCharacter {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        FirstUniqueCharacter obj = new FirstUniqueCharacter();
+
+        String s = "leetcode";   // Example input
+        int index = obj.firstUniqChar(s);
+
+        System.out.println("First unique character index: " + index);
     }
-    public int firstUniqChar(String s){
-        Map<Character,Integer> map=new HashMap<Character,Integer>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(map.containsKey(ch)){
-                map.put(ch,map.get(ch)+1);
-            }else{
-                map.put(ch,1);
-            }
+
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        for(int i=0;i<s.length();i++){
-             char ch=s.charAt(i);
-            if(map.get(ch)==1){
+
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1) {
                 return i;
             }
         }
+
+        return -1; 
     }
 }
