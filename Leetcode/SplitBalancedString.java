6eee5688512ba67e761edf61b764823a9bdbb4dc
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-10 23:24:29 +0530

    File is change

--- Leetcode/SplitBalancedString.java
+++ Leetcode/SplitBalancedString.java
@@ -2,5 +2,33 @@
 package solve_problems.Leetcode;
 
 public class SplitBalancedString {
-    
+    public static void main(String[] args) {
+        SplitBalancedString obj = new SplitBalancedString();
+
+        System.out.println(obj.balancedStringSplit("RLRRLLRLRL")); // 4
+        System.out.println(obj.balancedStringSplit("RLRRRLLRLL")); // 2
+        System.out.println(obj.balancedStringSplit("LLLLRRRR"));   // 1
+        System.out.println(obj.balancedStringSplit("RLRLRLRL"));   // 4
+    }
+
+    public int balancedStringSplit(String s) {
+        int balance = 0;
+        int count = 0;
+
+        for (char c : s.toCharArray()) {
+            if (c == 'L') {
+                balance++;
+            } else {
+                balance--;
+            }
+
+            // When balance becomes zero, a balanced string is found
+            if (balance == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
+
