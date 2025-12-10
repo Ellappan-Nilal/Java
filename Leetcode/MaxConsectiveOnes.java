8fcbd0b2bb23e896cbafcc3b37dac90d31f0db8d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-10 21:54:53 +0530

    FIle code is added

--- Leetcode/MaxConsectiveOnes.java
+++ Leetcode/MaxConsectiveOnes.java
@@ -2,5 +2,22 @@
 package solve_problems.Leetcode;
 
 public class MaxConsectiveOnes {
-    
+    public static void main(String[] args){
+        
+    }
+    public int findMaxConsecutiveones(int[] nums){
+        int max=0;
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==1){
+                count++;
+                max=Math.max(max,count);
+            
+            }
+            else{
+                count=0;
+            }
+        }
+        return max;
+    }
 }
