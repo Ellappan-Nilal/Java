7c7cbaaa44f37855323669ab7ede9545888da7c0
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-10 23:15:12 +0530

    File is added

---
+++ Leetcode/DetectCapital.java
@@ -0,0 +1,43 @@
+
+package solve_problems.Leetcode;
+
+public class DetectCapital {
+
+    public static void main(String[] args) {
+        DetectCapital obj = new DetectCapital();
+
+        System.out.println(obj.detectCapitalUse("USA"));      // true
+        System.out.println(obj.detectCapitalUse("leetcode")); // true
+        System.out.println(obj.detectCapitalUse("Google"));   // true
+        System.out.println(obj.detectCapitalUse("FlaG"));     // false
+    }
+
+    public boolean detectCapitalUse(String word) {
+        int uppercaseCount = 0;
+
+        for (char c : word.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                uppercaseCount++;
+            }
+        }
+
+        
+        if (uppercaseCount == word.length()) {
+            return true;
+        }
+
+        
+        if (uppercaseCount == 0) {
+            return true;
+        }
+
+        
+        if (uppercaseCount == 1 && Character.isUpperCase(word.charAt(0))) {
+            return true;
+        }
+
+        return false;
+    }
+}
+
+
