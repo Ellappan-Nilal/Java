fadfd55f78d9143040c750871bf5f1ac76f961e9
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-11 21:42:36 +0530

    File code is modify

--- Leetcode/BinarySearch.java
+++ Leetcode/BinarySearch.java
@@ -1,26 +1,41 @@
-
 package solve_problems.Leetcode;
 
 public class BinarySearch {
+
     public static void main(String[] args){
-        
+        BinarySearch bs = new BinarySearch();
+
+        int[] nums = {-5, -2, 0, 3, 7, 10, 12};
+        int target = 7;
+
+        int result = bs.search(nums, target);
+
+        if(result != -1){
+            System.out.println("Target found at index: " + result);
+        } else {
+            System.out.println("Target not found");
+        }
     }
-    public int search(int[] nums,int target){
+
+    public int search(int[] nums, int target){
         int mid;
-        int left=0;
-        int right=nums.length-1;
-        while(left<=right){
-            mid=left+(right-left)/2;
-            if(nums[mid]==target){
+        int left = 0;
+        int right = nums.length - 1;
+
+        while(left <= right){
+            mid = left + (right - left) / 2;
+
+            if(nums[mid] == target){
                 return mid;
             }
-            if(target<nums[mid]){
-                right=mid-1;
-            }
-            else{
-                left=mid+1;
+
+            if(target < nums[mid]){
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
+
         return -1;
     }
 }
