75156b2c709d8f9cc57d0313c9a53e00022a7d9d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-11 22:14:00 +0530

    File code is added

--- Leetcode/MiddleoftheLinkedlist.java
+++ Leetcode/MiddleoftheLinkedlist.java
@@ -1,6 +1,22 @@
 
 package solve_problems.Leetcode;
-
+public class ListNode{
+    int val;
+    ListNode next;
+    ListNode(){}
+    ListNode(int val){this.val=val;}
+    ListNode(int val.ListNode next){this.val=val;this.next=next; }
+}
 public class MiddleoftheLinkedlist {
-    
+    public static void main(String[] args){
+        
+    }
+    public ListNode middleNode(ListNode head){
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null&&fas.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+    }
 }
