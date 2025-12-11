d58158df94cfa0c3810ae5bc11b9e8abdef1920d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-11 21:39:10 +0530

    File is added

--- Leetcode/BinarySearch.java
+++ Leetcode/BinarySearch.java
@@ -2,5 +2,25 @@
 package solve_problems.Leetcode;
 
 public class BinarySearch {
-    
+    public static void main(String[] args){
+        
+    }
+    public int search(int[] nums,int target){
+        int mid;
+        int left=0;
+        int right=nums.length-1;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(target<nums[mid]){
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
 }
