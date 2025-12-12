4effa13495e193abbe29274e0bc764c0af7de4e0
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-12 21:31:08 +0530

    File code is added

--- Leetcode/ValidMountainArray.java
+++ Leetcode/ValidMountainArray.java
@@ -2,5 +2,26 @@
 package solve_problems.Leetcode;
 
 public class ValidMountainArray {
-    
+    public static void main(String[] args){
+        
+    }
+    public boolean validMountainArray(int[] arr){
+        int len=arr.length;
+        if(len<3){
+            return false;
+        }
+        int i=0,j=len-1;
+        while(i+1<len&&arr[i]<arr[i+1]){
+            i++;
+        }
+        while(j>0&&arr[j-1]>arr[j]){
+            j--;
+        }
+        if(i>0&&j<len-1){
+            if(i==j){
+                return true;
+            }
+        }
+        return false;
+    }
 }
