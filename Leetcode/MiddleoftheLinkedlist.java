3aabdeda505e61191fdc71b143134e9233f912aa
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-12 21:21:15 +0530

    File is change

--- Leetcode/MiddleoftheLinkedlist.java
+++ Leetcode/MiddleoftheLinkedlist.java
@@ -1,22 +1,47 @@
-
 package solve_problems.Leetcode;
-public class ListNode{
+
+class ListNode {
     int val;
     ListNode next;
-    ListNode(){}
-    ListNode(int val){this.val=val;}
-    ListNode(int val.ListNode next){this.val=val;this.next=next; }
-}
-public class MiddleoftheLinkedlist {
-    public static void main(String[] args){
-        
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
     }
-    public ListNode middleNode(ListNode head){
-        ListNode slow=head;
-        ListNode fast=head;
-        while(fast!=null&&fas.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MiddleoftheLinkedlist {
+
+    public static void main(String[] args) {
+
+        // Creating Linked List: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        MiddleoftheLinkedlist obj = new MiddleoftheLinkedlist();
+        ListNode middle = obj.middleNode(head);
+
+        System.out.println("Middle Node Value: " + middle.val);
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
+
+        return slow;  // Middle node
     }
 }
