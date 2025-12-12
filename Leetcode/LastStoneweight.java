f28bac6ba4e8ea032fbb326ecd8367b40deaec24
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-12 22:56:32 +0530

    File code is modify

--- Leetcode/LastStoneweight.java
+++ Leetcode/LastStoneweight.java
@@ -1,13 +1,49 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package solve_problems.Leetcode;
 
-/**
- *
- * @author Nilal
- */
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class LastStoneweight {
-    
+
+    public static void main(String[] args) {
+
+        int[] stones = {2, 7, 4, 1, 8, 1};
+
+        LastStoneweight obj = new LastStoneweight();
+        int result = obj.lastStoneWeight(stones);
+
+        System.out.println("Last Stone Weight: " + result);
+    }
+
+    public int lastStoneWeight(int[] stones) {
+
+        // Max Heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                return b - a;   // Max-heap
+            }
+        });
+
+        // Add all stones to heap
+        for (int stone : stones) {
+            pq.add(stone);
+        }
+
+        // Smash stones
+        while (pq.size() > 1) {
+            int stone1 = pq.poll();  // heaviest
+            int stone2 = pq.poll();  // second heaviest
+
+            if (stone1 != stone2) {
+                pq.add(stone1 - stone2);
+            }
+        }
+
+        // If no stones left, return 0
+        if (pq.isEmpty()) {
+            return 0;
+        }
+
+        return pq.poll();
+    }
 }
