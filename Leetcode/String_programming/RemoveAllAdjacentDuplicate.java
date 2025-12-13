0f6602f7387da3909a91c4dd19068985f244336f
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-13 21:18:29 +0530

    File cod eis modify

--- Leetcode/String_programming/RemoveAllAdjacentDuplicate.java
+++ Leetcode/String_programming/RemoveAllAdjacentDuplicate.java
@@ -1,25 +1,37 @@
 package String_programming;
 
-public class RemoveAllAdjacentDuplicate {
-    public static void main(String[] args){
+import java.util.*;
 
+public class RemoveAllAdjacentDuplicate {
+
+    public static void main(String[] args) {
+        RemoveAllAdjacentDuplicate obj = new RemoveAllAdjacentDuplicate();
+
+        String input = "abbaca";
+        String result = obj.removeDuplicate(input);
+
+        System.out.println("Input: " + input);
+        System.out.println("Output: " + result);
     }
-    public String removeDuplicate(String s){
-        Stack<Character> stack=new Stack<>();
-        for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            if(stack.isEmpty()){
-                stack.push(ch);
-            }else{
-                stack.pop();
-            }else{
+
+    public String removeDuplicate(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (!stack.isEmpty() && stack.peek() == ch) {
+                stack.pop(); // remove adjacent duplicate
+            } else {
                 stack.push(ch);
             }
         }
-        StringBuilder sb=new StringBuilder();
-        while(!stack.isEmpty()){
+
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
             sb.append(stack.pop());
         }
+
         return sb.reverse().toString();
     }
 }
