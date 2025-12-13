78dfccaad8d6afcc9f3012ce8c722593e7e278f4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-13 21:13:55 +0530

    File code is added

--- Leetcode/String_programming/RemoveAllAdjacentDuplicate.java
+++ Leetcode/String_programming/RemoveAllAdjacentDuplicate.java
@@ -1,5 +1,25 @@
 package String_programming;
 
 public class RemoveAllAdjacentDuplicate {
+    public static void main(String[] args){
 
+    }
+    public String removeDuplicate(String s){
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(stack.isEmpty()){
+                stack.push(ch);
+            }else{
+                stack.pop();
+            }else{
+                stack.push(ch);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!stack.isEmpty()){
+            sb.append(stack.pop());
+        }
+        return sb.reverse().toString();
+    }
 }
