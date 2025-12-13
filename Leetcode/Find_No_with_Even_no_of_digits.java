c7cc00282ea4ff240ac28315804f58cfa55008a2
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-13 21:37:08 +0530

    File is added

--- Leetcode/Find_No_with_Even_no_of_digits.java
+++ Leetcode/Find_No_with_Even_no_of_digits.java
@@ -1,3 +1,22 @@
 public class Find_No_with_Even_no_of_digits {
+    public static void main(String[] args){
 
+    }
+    public int findNo(int[] nums){
+        if(nums.length==0){
+            return 0;
+        }
+        int count =0;
+        for(int i=0;i<nums.length;i++){
+            int numsofDigits=0;
+            while(nums[i]!=0){
+                nums[i]=nums[i]/10;
+                numsofDigits++;
+            }
+            if(numsofDigits%2==0){
+                count++;
+            }
+        }
+        return count;
+    }
 }
