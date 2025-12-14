e7e1f9dc331e2a5fafbd5e3ebcfce65a743c1087
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-14 21:50:13 +0530

    File code is modified

--- Leetcode/MaximumProductofTwoElementsinanArray.java
+++ Leetcode/MaximumProductofTwoElementsinanArray.java
@@ -1,3 +1,23 @@
+import java.util.Arrays;
+
 public class MaximumProductofTwoElementsinanArray {
 
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 2};
+
+        MaximumProductofTwoElementsinanArray obj =
+                new MaximumProductofTwoElementsinanArray();
+
+        int result = obj.maxProduct(nums);
+        System.out.println("Maximum product: " + result);
+    }
+
+    public int maxProduct(int[] nums) {
+        Arrays.sort(nums);
+
+        int firstMax = nums[nums.length - 1];
+        int secondMax = nums[nums.length - 2];
+
+        return (firstMax - 1) * (secondMax - 1);
+    }
 }
