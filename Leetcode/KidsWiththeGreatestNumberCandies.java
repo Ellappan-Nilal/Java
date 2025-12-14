e5f5402668e5a5c0dfc6326a15c02db9918381c4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-14 21:39:19 +0530

    File code is added

--- Leetcode/KidsWiththeGreatestNumberCandies.java
+++ Leetcode/KidsWiththeGreatestNumberCandies.java
@@ -1,3 +1,23 @@
 public class KidsWiththeGreatestNumberCandies {
-
+    public static void main(String[] args){
+        
+    }
+    public List<Boolean> kidsWithCandies(int[] candies,int extraCandies){
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]>max){
+                max=candies[i];
+            }
+        }
+        List<Boolean> result=new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=max){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
 }
