94cdf6a19744dc62183b0417cc26671ac6729fcb
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-15 22:55:22 +0530

    code is added

--- Leetcode/RunningSumof1darray.java
+++ Leetcode/RunningSumof1darray.java
@@ -1,3 +1,12 @@
 public class RunningSumof1darray {
-    
+    public static void main(String[] args){
+
+    }
+    public int[] runningSum(int[] nums){
+        for(int i=0;i<=nums.length;i++){
+            sum=nums[i]+nums[i-1];
+            nums[i]=sum;
+        }
+        return nums;
+    }
 }
