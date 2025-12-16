e50e44f032e1ec0fdb9e7873f01640c10fe0d03a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-16 22:13:34 +0530

    File code is added

--- Leetcode/SortArraybyIncreasingFrequency.java
+++ Leetcode/SortArraybyIncreasingFrequency.java
@@ -1,31 +1,49 @@
-public class SortArraybyIncreasingFrequency {
-    public static void main(String[] args){
+import java.util.*;
 
+public class SortArraybyIncreasingFrequency {
+
+    public static void main(String[] args) {
+        SortArraybyIncreasingFrequency obj = new SortArraybyIncreasingFrequency();
+
+      
+        int[] nums = {1, 1, 2, 2, 2, 3};
+
+       
+        int[] result = obj.frequencySort(nums);
+
+       
+        System.out.println("Sorted Array by Increasing Frequency:");
+        System.out.println(Arrays.toString(result));
     }
-    public int[] frequencySort(int[] nums){
-        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+
+    public int[] frequencySort(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
         }
-        List<Integer> list=new ArrayList<Integer>(map.keySet());
-        Collection.sort(list, (a,b)->{
-            if(map.get(a)==map.get(b)){
-                return b-a;
-            }
-            else{
-                return map.get(a)-map.get(b);
+
+ 
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list, (a, b) -> {
+            if (map.get(a).equals(map.get(b))) {
+                return b - a; // decreasing value
+            } else {
+                return map.get(a) - map.get(b); // increasing frequency
             }
         });
-        int result[]=new int[nums.length];
-        int index=0;
-        for(int num:list){
-            for(int i=0;i<map.get(num);i++){
-                result[index] =num;
+
+        
+        int[] result = new int[nums.length];
+        int index = 0;
+
+        for (int num : list) {
+            for (int i = 0; i < map.get(num); i++) {
+                result[index++] = num;
             }
         }
+
+        return result;
     }
 }
