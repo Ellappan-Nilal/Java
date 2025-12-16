2baa01bbb1d83239ddd028659670b268273936d7
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-16 22:06:51 +0530

    Map adding function

--- Leetcode/SortArraybyIncreasingFrequency.java
+++ Leetcode/SortArraybyIncreasingFrequency.java
@@ -1,3 +1,15 @@
 public class SortArraybyIncreasingFrequency {
+    public static void main(String[] args){
 
+    }
+    public int[] frequencySort(int[] nums){
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+    }
 }
