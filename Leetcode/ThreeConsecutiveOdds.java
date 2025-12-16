8e7d464e5e1c03ebacc52253bb44d4f38a28f266
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-16 21:51:44 +0530

    File code is modify

--- Leetcode/ThreeConsecutiveOdds.java
+++ Leetcode/ThreeConsecutiveOdds.java
@@ -1,21 +1,32 @@
 public class ThreeConsecutiveOdds {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        ThreeConsecutiveOdds obj = new ThreeConsecutiveOdds();
+
+        
+        int[] arr = {2, 6, 4, 1, 3, 5};
+
+        
+        boolean result = obj.ConsecutiveOdds(arr);
+
+       
+        System.out.println("Contains three consecutive odds: " + result);
     }
-    public boolean ConsecutiveOdds (int[] arr) {
-        int count =0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]%2==1){
+
+    public boolean ConsecutiveOdds(int[] arr) {
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] % 2 == 1) {
                 count++;
+            } else {
+                count = 0;
             }
-            else{
-                count=0;
-            }
-            if(count==3){
+
+            if (count == 3) {
                 return true;
             }
-           
         }
-         return false;
+        return false;
     }
 }
