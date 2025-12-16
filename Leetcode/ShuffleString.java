534c079b1fceb564447d0dd2bcf08502dd2d6339
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-16 21:21:23 +0530

    File is modify

--- Leetcode/ShuffleString.java
+++ Leetcode/ShuffleString.java
@@ -1,26 +1,39 @@
 public class ShuffleString {
-    public static void main(String[] args){
 
+    public static void main(String[] args) {
+        ShuffleString obj = new ShuffleString();
+
+        
+        String s = "codeleet";
+        int[] indices = {4, 5, 6, 7, 0, 1, 2, 3};
+
+       
+        String result = obj.shuffle(s, indices);
+
+        
+        System.out.println("Shuffled String: " + result);
     }
-    public String shuffle(String s, int[] indices){
-        if(s==null||s.length()==0){
+
+    public String shuffle(String s, int[] indices) {
+        if (s == null || s.length() == 0) {
             return "";
         }
-        if(s.length()!=indices.length){
+        if (s.length() != indices.length) {
             return "";
-
         }
-        char chars[]=new char[s.length()];
-        for(int i=0;i<indices.length;i++){
-            int pos=indices[i];
-            chars[pos]=s.charAt(i);
-        }
-        String result="";
-        for(int i=0;i<chars.length;i++){
-            result+=chars[i];
-        }
-        return result;
 
+        char[] chars = new char[s.length()];
 
+        for (int i = 0; i < indices.length; i++) {
+            int pos = indices[i];
+            chars[pos] = s.charAt(i);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char c : chars) {
+            result.append(c);
+        }
+
+        return result.toString();
     }
 }
