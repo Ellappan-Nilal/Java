db8083ca2ffc076095b5e3051a1bbd558df730a4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-17 17:41:00 +0530

    File code is added

--- Leetcode/SumofUniqueElements.java
+++ Leetcode/SumofUniqueElements.java
@@ -1,8 +1,24 @@
+import java.util.Map;
+
 public class SumofUniqueElements {
     public static void main(String[] args){
         
     }
     public int sumOfUnique(int[] nums){
-        Map<Integer,Integer> map=new HashMap
+        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else{
+                map.put(nums[i],1);
+            }
+        }
+        int sum=0;
+        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
+            if(entry.getValue()==1){
+                sum+=entry.getKey();
+            }
+        }
+        return sum;
     }
 }
