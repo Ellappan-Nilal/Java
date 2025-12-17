e72a4cc57b9153a57ba7abe50e5f86245a2caadf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-17 17:43:27 +0530

    File code is modify

--- Leetcode/SumofUniqueElements.java
+++ Leetcode/SumofUniqueElements.java
@@ -1,22 +1,28 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class SumofUniqueElements {
-    public static void main(String[] args){
-        
+
+    public static void main(String[] args) {
+        SumofUniqueElements obj = new SumofUniqueElements();
+
+        int[] nums = {1, 2, 3, 2};
+        int result = obj.sumOfUnique(nums);
+
+        System.out.println("Sum of unique elements: " + result);
     }
-    public int sumOfUnique(int[] nums){
-        Map<Integer,Integer> map=new HashMap<Integer,Integer>();
-        for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i],map.get(nums[i])+1);
-            }else{
-                map.put(nums[i],1);
-            }
+
+    public int sumOfUnique(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
-        int sum=0;
-        for(Map.Entry<Integer,Integer> entry:map.entrySet()){
-            if(entry.getValue()==1){
-                sum+=entry.getKey();
+
+        int sum = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                sum += entry.getKey();
             }
         }
         return sum;
