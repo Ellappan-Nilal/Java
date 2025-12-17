4a7aa06916cb54217b2ccacc1bd25666e0c81512
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-17 17:26:05 +0530

    File is created

--- Leetcode/SumofUniqueElements.java
+++ Leetcode/SumofUniqueElements.java
@@ -2,4 +2,7 @@ public class SumofUniqueElements {
     public static void main(String[] args){
         
     }
+    public int sumOfUnique(int[] nums){
+        Map<Integer,Integer> map=new HashMap
+    }
 }
