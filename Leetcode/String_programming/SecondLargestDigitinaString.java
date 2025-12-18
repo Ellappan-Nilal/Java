767811945dcef2666f3eecc866d3484274a66f7e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-18 21:32:41 +0530

    File is added

--- Leetcode/String_programming/SecondLargestDigitinaString.java
+++ Leetcode/String_programming/SecondLargestDigitinaString.java
@@ -2,4 +2,34 @@ package String_programming;
 
 public class SecondLargestDigitinaString {
 
+    public static void main(String[] args) {
+        SecondLargestDigitinaString obj = new SecondLargestDigitinaString();
+
+        String input = "abc12345def6789";
+        int result = obj.secondHighest(input);
+
+        System.out.println("Second largest digit: " + result); 
+    }
+
+    public int secondHighest(String s) {
+        int largest = -1;
+        int secondLargest = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (Character.isDigit(ch)) {
+                int digit = ch - '0';
+
+                if (digit > largest) {
+                    secondLargest = largest;
+                    largest = digit;
+                } else if (digit < largest && digit > secondLargest) {
+                    secondLargest = digit;
+                }
+            }
+        }
+
+        return secondLargest;
+    }
 }
