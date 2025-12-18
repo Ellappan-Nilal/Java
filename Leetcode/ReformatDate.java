f2ff6400e20b2a23888201bf9147a32fec681abf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-18 21:36:25 +0530

    File is change

--- Leetcode/ReformatDate.java
+++ Leetcode/ReformatDate.java
@@ -11,7 +11,7 @@ public class ReformatDate {
 
         System.out.println(output); 
     }
-
+    
     public String reformatDate(String date) {
                
         String year = date.substring(date.length() - 4);
