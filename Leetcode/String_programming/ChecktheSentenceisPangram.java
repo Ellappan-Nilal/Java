e7e16d252d8a9acbdf44d6610c9c076640d6532e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-19 15:25:32 +0530

    File code is added

--- Leetcode/String_programming/ChecktheSentenceisPangram.java
+++ Leetcode/String_programming/ChecktheSentenceisPangram.java
@@ -1,5 +1,14 @@
 package String_programming;
 
 public class ChecktheSentenceisPangram {
-
+    public static  void main(String[] args){
+        Set<Character> set=new HashSet<>();
+        for(int i=0;i<sentence.length();i++){
+            set.add(sentence.charAt(i));
+        }
+        if(set.size()==26){
+            System.out.println("true"); 
+        }
+        return false;
+    }
 }
