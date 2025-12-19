90eed82aed7ba9b0d0b04f54f28040bacf480b7b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-19 15:36:06 +0530

    File code is modify

--- Leetcode/String_programming/ChecktheSentenceisPangram.java
+++ Leetcode/String_programming/ChecktheSentenceisPangram.java
@@ -1,14 +1,28 @@
 package String_programming;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ChecktheSentenceisPangram {
-    public static  void main(String[] args){
-        Set<Character> set=new HashSet<>();
-        for(int i=0;i<sentence.length();i++){
-            set.add(sentence.charAt(i));
+
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+
+        boolean result = checkIfPangram(sentence);
+        System.out.println(result);
+    }
+
+    public static boolean checkIfPangram(String sentence) {
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < sentence.length(); i++) {
+            char ch = sentence.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+                set.add(ch);
+            }
         }
-        if(set.size()==26){
-            return true;    
-        }
-        return false;
+
+        return set.size() == 26;
     }
 }
