e16b415cbe8185f37de7250e9cdb2ed17e971b77
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-19 16:01:01 +0530

    File code is added

--- Leetcode/String_programming/SortSentence.java
+++ Leetcode/String_programming/SortSentence.java
@@ -1,5 +1,18 @@
 package String_programming;
 
 public class SortSentence {
+    public static void main(String s){
 
+    }
+    public String sortSentence(String s) {
+        String[] words = s.split(" ");
+        String[] sorted = new String[words.length];
+
+        for (String word : words) {
+            int index = word.charAt(word.length() - 1) - '1';
+            sorted[index] = word.substring(0, word.length() - 1);
+        }
+
+        return String.join(" ", sorted);
+    }
 }
