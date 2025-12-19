d5b7bea6de9fb7a03d24b0b88822a14ca4fa9e49
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-19 16:07:28 +0530

    File code is added

--- Leetcode/ConcatenationofArray.java
+++ Leetcode/ConcatenationofArray.java
@@ -1,3 +1,24 @@
 public class ConcatenationofArray {
+    public static void main(String[] args) {
+        ConcatenationofArray obj = new ConcatenationofArray();
 
+        int[] nums = {1, 2, 3};
+        int[] result = obj.getConcatenation(nums);
+
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = nums[i];
+            result[i + n] = nums[i];
+        }
+
+        return result;
+    }   
 }
