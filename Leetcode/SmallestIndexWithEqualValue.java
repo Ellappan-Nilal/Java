5953387706b1e69643451db57c69d598c4eae2c6
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-19 16:19:44 +0530

    File code is added

--- Leetcode/SmallestIndexWithEqualValue.java
+++ Leetcode/SmallestIndexWithEqualValue.java
@@ -1,3 +1,19 @@
 public class SmallestIndexWithEqualValue {
+    public static void main(String[] args) {
+        SmallestIndexWithEqualValue obj = new SmallestIndexWithEqualValue();
 
+        int[] nums = {0, 2, 3, 4, 5};
+        int result = obj.smallestEqual(nums);
+
+        System.out.println(result); 
+    }
+
+    public int smallestEqual(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 10 == nums[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
 }
