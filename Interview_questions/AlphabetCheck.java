3b6c1a7f8aadb3a6ebfa4a586af55034f180167e
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-21 13:48:41 +0530

    FIle is added

---
+++ Interview_questions/AlphabetCheck.java
@@ -0,0 +1,16 @@
+
+package solve_problems.Interview_questions;
+
+import java.util.Scanner;
+
+public class AlphabetCheck {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+           System.out.print(ch+" is Alpabet character");
+        }else{
+            System.out.print(ch+" is Not Alpabet character ");
+        }
+    }
+}
