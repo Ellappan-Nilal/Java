d39a0e8a155a515728674b15a64505000956e359
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-21 11:57:49 +0530

    File is added

---
+++ Interview_questions/GCDOfTwoNo.java
@@ -0,0 +1,30 @@
+
+package solve_problems.Interview_questions;
+
+import java.util.Scanner;
+
+public class GCDOfTwoNo {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.print(gcd(n1,n2));
+        
+    }
+    public static int gcd(int a,int b){
+        if (a == 0)
+            return b;
+        if (b == 0)
+            return a;
+
+        
+        if (a == b)
+            return a;
+
+        
+        if (a > b)
+            return gcd(a - b, b);
+        return gcd(a, b - a);
+        
+    }
+}
