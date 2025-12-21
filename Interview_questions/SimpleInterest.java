1c8b32e6d02585a02d2c181cf0bd9638537fac63
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-21 07:01:01 +0530

    file code is added

---
+++ Interview_questions/SimpleInterest.java
@@ -0,0 +1,11 @@
+package Interview_questions;
+
+public class SimpleInterest {
+    public static void main(String[] args){
+        double p=1000;
+        double r=3;
+        double t=5;
+        double si=(p*r*t)/100;
+        System.out.println("Simple Interest is: "+si);
+    }
+}
