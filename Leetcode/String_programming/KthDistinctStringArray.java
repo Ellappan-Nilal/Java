3e9afdb4f9773e730aedad3e63dcea1ac841b0d0
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-23 11:46:57 +0530

    File is added

--- Leetcode/String_programming/KthDistinctStringArray.java
+++ Leetcode/String_programming/KthDistinctStringArray.java
@@ -1,5 +1,9 @@
 package String_programming;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class KthDistinctStringArray {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
@@ -7,19 +11,29 @@ public class KthDistinctStringArray {
         String[] arr=new String[n];
         for(int i=0;i<n;i++){
             arr[i]=sc.next();
-        }       
-    }
-    public String kthDistinct(String[] arr, int k) {
-        int distinctCount = 0;
+        }
+        int k = sc.nextInt();
+        sc.close();
 
-        for (String str : arr) {
-            if (isDistinct(arr, str)) {
+        String ans = kthDistinct(arr, k);
+        System.out.println(ans);
+    }
+
+    public static String kthDistinct(String[] arr, int k) {
+        Map<String, Integer> counts = new LinkedHashMap<>();
+        for (String s : arr) {
+            counts.put(s, counts.getOrDefault(s, 0) + 1);
+        }
+
+        int distinctCount = 0;
+        for (Map.Entry<String, Integer> e : counts.entrySet()) {
+            if (e.getValue() == 1) {
                 distinctCount++;
                 if (distinctCount == k) {
-                    return str;
+                    return e.getKey();
                 }
             }
         }
         return "";
-    }   
+    }
 }
