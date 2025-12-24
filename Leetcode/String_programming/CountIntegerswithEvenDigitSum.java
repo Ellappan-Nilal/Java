043b3217da3d193f09cc610c034f4c1a38b6850b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-24 09:51:33 +0530

    FIle is modify

--- Leetcode/String_programming/CountIntegerswithEvenDigitSum.java
+++ Leetcode/String_programming/CountIntegerswithEvenDigitSum.java
@@ -1,5 +1,35 @@
 package String_programming;
 
 public class CountIntegerswithEvenDigitSum {
+    public static void main(String[] args){
+        int low = 30;
+        int high = 45;
 
+        int result = countEven(low, high);
+
+        System.out.println("Count of integers with even digit sum between " + low + " and " + high + ": " + result);
+    }
+
+    public static int countEven(int low, int high) {
+        int count = 0;
+
+        for (int num = low; num <= high; num++) {
+            if (isDigitSumEven(num)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static boolean isDigitSumEven(int num) {
+        int sum = 0;
+
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+
+        return sum % 2 == 0;
+    }
 }
