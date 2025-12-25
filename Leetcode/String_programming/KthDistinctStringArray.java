f4e6322a06be835ed22d55714a83c0f10b4717eb
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-25 21:36:29 +0530

    File is change

--- Leetcode/String_programming/KthDistinctStringArray.java
+++ Leetcode/String_programming/KthDistinctStringArray.java
@@ -18,13 +18,11 @@ public class KthDistinctStringArray {
         String ans = kthDistinct(arr, k);
         System.out.println(ans);
     }
-
     public static String kthDistinct(String[] arr, int k) {
         Map<String, Integer> counts = new LinkedHashMap<>();
         for (String s : arr) {
             counts.put(s, counts.getOrDefault(s, 0) + 1);
         }
-
         int distinctCount = 0;
         for (Map.Entry<String, Integer> e : counts.entrySet()) {
             if (e.getValue() == 1) {
