3f03dbcca8ba062240ee9326a00a4ad67a954bab
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-26 10:30:05 +0530

    File code is modify

--- Leetcode/CheckifArrayisGood.java
+++ Leetcode/CheckifArrayisGood.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class CheckifArrayisGood {
     public static void main(String[] args) {
         int[] arr = {3, 2, 1, 4, 5};
@@ -6,21 +8,16 @@ public class CheckifArrayisGood {
     }
 
     public static boolean isGoodArray(int[] arr) {
-        int n = arr.length;
-        boolean[] seen = new boolean[n + 1];
+        Arrays.sort(arr);
+        int max = arr.length - 1;
 
-        for (int num : arr) {
-            if (num <= n) {
-                seen[num] = true;
-            }
+        int[] expected = new int[max + 1];
+        int n = expected.length;
+        for (int i = 0; i < n; i++) {
+            expected[i] = i + 1;
         }
-
-        for (int i = 1; i <= n; i++) {
-            if (!seen[i]) {
-                return false;
-            }
-        }
-
-        return true;
+        expected[n - 1] = max;
+        
+        return Arrays.equals(arr, expected);
     }   
 }
