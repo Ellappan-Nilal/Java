29d7fa577ee9b43d1b5375e542e5557fba6762d3
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-27 15:50:32 +0530

    File code is added

--- Leetcode/AccountBalanceAfterRoundedPurchase.java
+++ Leetcode/AccountBalanceAfterRoundedPurchase.java
@@ -1,3 +1,12 @@
 public class AccountBalanceAfterRoundedPurchase {
+    public static void main(String[] args) {
+        int purchaseAmount = 23; // Example purchase amount
+        int finalBalance = accountBalanceAfterRoundedPurchase(purchaseAmount);
+        System.out.println("Final account balance: " + finalBalance);
+    }
 
+    public static int accountBalanceAfterRoundedPurchase(int purchaseAmount) {
+        int roundedPurchase = ((purchaseAmount + 5) / 10) * 10;
+        return 100 - roundedPurchase;
+    }
 }
