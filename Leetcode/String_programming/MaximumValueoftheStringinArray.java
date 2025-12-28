7a07c32023d1e90f71ce1084e7829eaab46d392a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-28 19:08:42 +0530

    File code is added

--- Leetcode/String_programming/MaximumValueoftheStringinArray.java
+++ Leetcode/String_programming/MaximumValueoftheStringinArray.java
@@ -1,5 +1,31 @@
 package String_programming;
-
 public class MaximumValueoftheStringinArray {
+    public static void main(String[] args) {
+        String[] strs = {"alic3", "bob", "3", "4", "00000"};
+        int result = maximumValue(strs);
+        System.out.println("Maximum value in the array: " + result);
+    }
 
+    public static int maximumValue(String[] strs) {
+        int maxVal = 0;
+        for (String str : strs) {
+            int val;
+            if (isNumeric(str)) {
+                val = Integer.parseInt(str);
+            } else {
+                val = str.length();
+            }
+            maxVal = Math.max(maxVal, val);
+        }
+        return maxVal;
+    }
+
+    private static boolean isNumeric(String str) {
+        for (char c : str.toCharArray()) {
+            if (!Character.isDigit(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
