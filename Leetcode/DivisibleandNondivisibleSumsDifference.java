b114b7ad2f0908952c103666515a0ff13c8f15c4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-28 19:18:48 +0530

    File is modify

--- Leetcode/DivisibleandNondivisibleSumsDifference.java
+++ Leetcode/DivisibleandNondivisibleSumsDifference.java
@@ -1,3 +1,22 @@
 public class DivisibleandNondivisibleSumsDifference {
+    public static void main(String[] args){
+        int[] nums={10,12,8,6};
+        int k=5;
+        int result=divisiblenondivisiblediff(nums,k);
+        System.out.println("Difference between divisible and non-divisible sums: " + result);
+    }
+    public static int divisiblenondivisiblediff(int[] nums, int k) {
+        int divisibleSum = 0;
+        int nonDivisibleSum = 0;
 
+        for (int num : nums) {
+            if (num % k == 0) {
+                divisibleSum += num;
+            } else {
+                nonDivisibleSum += num;
+            }
+        }
+
+        return Math.abs(divisibleSum - nonDivisibleSum);
+    }
 }
