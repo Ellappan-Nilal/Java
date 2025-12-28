fbd08c2f6375d3434f17d7fe911591b42bf25de4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2025-12-28 18:47:34 +0530

    File is added

--- Leetcode/ConvertTemperature.java
+++ Leetcode/ConvertTemperature.java
@@ -1,5 +1,15 @@
 public class ConvertTemperature {
     public static void main(String[] args){
-        
+        double cels=23;
+        System.out.print(onverttemp(cels);)
+
+    }
+    public double[] converttemp(double cels){
+        double ans[]=new double[2];
+        double kel=cels+273.15;
+        double farn=(cels*1.80)+32.00;
+        ans[0]=kel;
+        ans[1]=farn;
+        return ans;
     }
 }
