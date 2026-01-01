edbf256a1e8e47acf3ef651a7df443d003c4b068
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-01 15:01:20 +0530

    File code is added

--- Leetcode/CalculateDelayedArrivalTime.java
+++ Leetcode/CalculateDelayedArrivalTime.java
@@ -1,3 +1,14 @@
 public class CalculateDelayedArrivalTime {
-
+    public static void main(String[] args){
+        int arrivalTime = 22;
+        int delayedTime = 5;
+        System.out.print(calculateDelayedArrivalTime(arrivalTime, delayedTime));
+    }
+    public static int calculateDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int totalTime = arrivalTime + delayedTime;
+        if (totalTime >= 24) {
+            totalTime -= 24;
+        }
+        return totalTime;
+    }
 }
