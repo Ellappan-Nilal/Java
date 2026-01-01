502fd1166b3512524efac9e33603ba91618f9b26
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-01 14:58:57 +0530

    File is added

--- Leetcode/SumofMultiples.java
+++ Leetcode/SumofMultiples.java
@@ -1,3 +1,15 @@
 public class SumofMultiples {
-
+    public static void main(String[] args){
+        int n=10;
+        System.out.print(sumOfMultiples(n));
+    }
+    public static int sumOfMultiples(int n) {
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(i%3==0 || i%5==0 || i%7==0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
 }
