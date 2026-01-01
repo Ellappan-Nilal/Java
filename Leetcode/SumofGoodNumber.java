fb0b15f8411065b549deaeed3539778db7022f8b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-01 14:48:02 +0530

    File is change

--- Leetcode/SumofGoodNumber.java
+++ Leetcode/SumofGoodNumber.java
@@ -1,15 +1,22 @@
-public class SumofGoodNumber {      
-     
-    
-    public static void main(String[] args){
-        SumofGoodNumber sgn=new SumofGoodNumber();
-        System.out.print(sgn.SumofGoodNumber(20));
-    }
-    public int SumofGoodNumber(int n){
-        int sum=0;
-        for(int i=1;i<=n;i++){
-            if(isGoodNumber(i)){
-                sum+=i;
+public class SumOfGoodNumber {
+
+    public int sumOfGoodNumber(int[] nums, int k) {
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            boolean isGood = true;
+
+            if (i - k >= 0 && nums[i] <= nums[i - k]) {
+                isGood = false;
+            }
+
+            
+            if (i + k < nums.length && nums[i] <= nums[i + k]) {
+                isGood = false;
+            }
+
+            if (isGood) {
+                sum += nums[i];
             }
         }
         return sum;
