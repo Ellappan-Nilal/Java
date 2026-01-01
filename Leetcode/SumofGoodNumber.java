7f2a6f1f839408e51f64d72a94072de2fd29288a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-01 14:38:07 +0530

    File is change

--- Leetcode/SumofGoodNumber.java
+++ Leetcode/SumofGoodNumber.java
@@ -1,3 +1,17 @@
-public class SumofGoodNumber {
-
+public class SumofGoodNumber {      
+     
+    
+    public static void main(String[] args){
+        SumofGoodNumber sgn=new SumofGoodNumber();
+        System.out.print(sgn.SumofGoodNumber(20));
+    }
+    public int SumofGoodNumber(int n){
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            if(isGoodNumber(i)){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
 }
