e04a3ed2132d3e23d3500221937ef163202676ae
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-02 21:11:23 +0530

    File is change

--- Leetcode/LongerContiguousOnesthanZeros.java
+++ Leetcode/LongerContiguousOnesthanZeros.java
@@ -1,3 +1,30 @@
 public class LongerContiguousOnesthanZeros {
+    public static void main(String[] args){
+        int[] nums = {1101, 1110, 0011, 0000};
+        System.out.print(longerContiguousOnesThanZeros(nums));
+    }
+    public static boolean longerContiguousOnesThanZeros(int[] nums) {
+        for (int num : nums) {
+            String binaryString = Integer.toBinaryString(num);
+            int maxOnes = 0, maxZeros = 0;
+            int currentOnes = 0, currentZeros = 0;
 
+            for (char bit : binaryString.toCharArray()) {
+                if (bit == '1') {
+                    currentOnes++;
+                    maxOnes = Math.max(maxOnes, currentOnes);
+                    currentZeros = 0;
+                } else {
+                    currentZeros++;
+                    maxZeros = Math.max(maxZeros, currentZeros);
+                    currentOnes = 0;
+                }
+            }
+
+            if (maxZeros >= maxOnes) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
