2250981149d6b7480b0515355a448e8d7c75c4c7
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-02 21:19:23 +0530

    File is change

--- Leetcode/String_programming/ConsecutiveCharacters.java
+++ Leetcode/String_programming/ConsecutiveCharacters.java
@@ -1,5 +1,23 @@
 package String_programming;
 
 public class ConsecutiveCharacters {
+    public static void main(String[] args){
+        String s = "leetcode";
+        System.out.print(maxPower(s));
+    }
+    public static int maxPower(String s) {
+        int maxCount = 1;
+        int currentCount = 1;
 
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 1;
+            }
+        }
+
+        return maxCount;
+    }
 }
