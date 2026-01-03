c21d88df0bc54d98ac67b17029c7ba63a7c62c52
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-03 17:23:29 +0530

    File code is added

--- Leetcode/String_programming/CountingWordsWithagivenPrefix.java
+++ Leetcode/String_programming/CountingWordsWithagivenPrefix.java
@@ -1,5 +1,21 @@
 package String_programming;
 
 public class CountingWordsWithagivenPrefix {
+    public static void main(String[] args) {
+        String[] words = {"pay", "attention", "practice", "attend"};
+        String pref = "at";
+        System.out.println(prefixCount(words, pref));
+    }
+    public static int prefixCount(String[] words, String pref) {
+        int count = 0;
+        int prefLength = pref.length();
 
+        for (String word : words) {
+            if (word.length() >= prefLength && word.substring(0, prefLength).equals(pref)) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
