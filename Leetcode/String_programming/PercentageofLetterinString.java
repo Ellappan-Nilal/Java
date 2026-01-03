eff310fa5b410f3e0b786e78aa80a41145cc5fc7
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-03 17:20:18 +0530

    File is change

--- Leetcode/String_programming/PercentageofLetterinString.java
+++ Leetcode/String_programming/PercentageofLetterinString.java
@@ -1,5 +1,22 @@
 package String_programming;
 
 public class PercentageofLetterinString {
+    public static void main(String[] args) {
+        String s = "foobar";
+        char letter = 'o';
+        System.out.println(percentageLetter(s, letter));
+    }
 
+    public static int percentageLetter(String s, char letter) {
+        int count = 0;
+        int length = s.length();
+
+        for (char c : s.toCharArray()) {
+            if (c == letter) {
+                count++;
+            }
+        }
+
+        return (count * 100) / length;
+    }
 }
