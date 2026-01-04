96cd56ce3363bbee0d16129c584352dabcd2873a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-04 19:00:05 +0530

    File is added

--- Leetcode/String_programming/IsSubsequence1.java
+++ Leetcode/String_programming/IsSubsequence1.java
@@ -1,5 +1,23 @@
 package String_programming;
 
 public class IsSubsequence1 {
+    public static void main(String[] args) {
+        String s = "abc";
+        String t = "ahbgdc";
+        System.out.println(isSubsequence(s, t));
+    }
 
+    public static boolean isSubsequence(String s, String t) {
+        int pointerS = 0;
+        int pointerT = 0;
+
+        while (pointerS < s.length() && pointerT < t.length()) {
+            if (s.charAt(pointerS) == t.charAt(pointerT)) {
+                pointerS++;
+            }
+            pointerT++;
+        }
+
+        return pointerS == s.length();
+    }
 }
