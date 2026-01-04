2485988fc3a03e609834ed87b22ee266447a0cb3
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-04 19:06:44 +0530

    File is added

--- Leetcode/String_programming/GreatestCommonDivisorofStrings.java
+++ Leetcode/String_programming/GreatestCommonDivisorofStrings.java
@@ -1,5 +1,23 @@
 package String_programming;
 
 public class GreatestCommonDivisorofStrings {
+    public static void main(String[] args) {
+        String str1 = "ABABAB";
+        String str2 = "ABAB";
+        System.out.println(gcdOfStrings(str1, str2));
+    }
+    public static String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
 
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
+    }
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return gcd(b, a % b);
+    }
 }
