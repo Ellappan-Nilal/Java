05f27a37d89dc778c241c187864e480233b46de4
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-04 18:47:18 +0530

    File is change

--- Leetcode/FindChampionI.java
+++ Leetcode/FindChampionI.java
@@ -1,3 +1,15 @@
 public class FindChampionI {
-
+    public static void main(String[] args) {
+        int[] scores = {10, 20, 30, 40, 50};
+        System.out.println(findChampion(scores));
+    }   
+    public static int findChampion(int[] scores) {
+        int maxScore = Integer.MIN_VALUE;
+        for (int score : scores) {
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+        return maxScore;
+    }
 }
