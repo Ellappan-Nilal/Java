0ca2ce9a0968ca384ed70cfec2b77e47a270afb9
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-05 19:34:34 +0530

    File code is addded

--- Leetcode/MinimumCommonValue.java
+++ Leetcode/MinimumCommonValue.java
@@ -1,3 +1,23 @@
 public class MinimumCommonValue {
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,4};
+        int[] nums2 = {2,4,6,8};
+        System.out.println(getCommon(nums1, nums2));
+    }
+    public static int getCommon(int[] nums1, int[] nums2) {
+        int pointer1 = 0;
+        int pointer2 = 0;
 
+        while (pointer1 < nums1.length && pointer2 < nums2.length) {
+            if (nums1[pointer1] == nums2[pointer2]) {
+                return nums1[pointer1];
+            } else if (nums1[pointer1] < nums2[pointer2]) {
+                pointer1++;
+            } else {
+                pointer2++;
+            }
+        }
+
+        return -1;
+    }
 }
