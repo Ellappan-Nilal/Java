2bd28a7d340bc1a9e6c142b04a79c0ba43c869ac
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-05 19:32:02 +0530

    File code is added

--- Leetcode/MaximumCountofPositiveIntegerandNegativeInteger.java
+++ Leetcode/MaximumCountofPositiveIntegerandNegativeInteger.java
@@ -1,3 +1,20 @@
 public class MaximumCountofPositiveIntegerandNegativeInteger {
+    public static void main(String[] args) {
+        int[] nums = {-2, -1, -1, 1, 2, 3};
+        System.out.println(maximumCount(nums));
+    }
+    public static int maximumCount(int[] nums) {
+        int positiveCount = 0;
+        int negativeCount = 0;
 
+        for (int num : nums) {
+            if (num > 0) {
+                positiveCount++;
+            } else if (num < 0) {
+                negativeCount++;
+            }
+        }
+
+        return Math.max(positiveCount, negativeCount);
+    }
 }
