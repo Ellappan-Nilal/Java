8070bf92b7970f7f83f1c5326b901b4aff54cab0
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-07 22:30:49 +0530

    file is change

--- Leetcode/Majority_Element.java
+++ Leetcode/Majority_Element.java
@@ -15,9 +15,7 @@ public class Majority_Element {
     }
 
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-
-       
+        Map<Integer, Integer> map = new HashMap<>();       
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 map.put(nums[i], map.get(nums[i]) + 1);
