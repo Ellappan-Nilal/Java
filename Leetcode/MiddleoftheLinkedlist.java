8201cccb8f6750c0c016bedddc6db8e3e3b9cbc8
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-09 22:04:44 +0530

    File is change

--- Leetcode/MiddleoftheLinkedlist.java
+++ Leetcode/MiddleoftheLinkedlist.java
@@ -5,17 +5,14 @@ class ListNode {
     ListNode next;
 
     ListNode() {}
-
     ListNode(int val) {
         this.val = val;
     }
-
     ListNode(int val, ListNode next) {
         this.val = val;
-        this.next = next;
+        this.next = next;s
     }
 }
-
 public class MiddleoftheLinkedlist {
 
     public static void main(String[] args) {
