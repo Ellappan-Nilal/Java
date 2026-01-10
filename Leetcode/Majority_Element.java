663ab5200e767eea04830f0f28e855cd06570a0d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-10 21:23:26 +0530

    FIle is change

--- Leetcode/Majority_Element.java
+++ Leetcode/Majority_Element.java
@@ -19,6 +19,7 @@ public class Majority_Element {
                 map.put(nums[i], 1);
             }
         }
+        
         int majority = nums.length / 2;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
             if (entry.getValue() > majority) {
