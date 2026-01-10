554c5a8eb9d9d03afc5711fbbbae05e3f548ee70
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-10 21:13:08 +0530

    File is change

--- Leetcode/Valid_Anagram.java
+++ Leetcode/Valid_Anagram.java
@@ -12,7 +12,6 @@ public class Valid_Anagram {
         String t2 = "car";
         System.out.println(obj.isAnagram(s2, t2)); // false
     }
-
     public boolean isAnagram(String s, String t) {
 
         if (s.length() != t.length()) {
@@ -20,18 +19,15 @@ public class Valid_Anagram {
         }
 
         int[] count = new int[26];
-
        
         for (int i = 0; i < s.length(); i++) {
             count[s.charAt(i) - 'a']++;
         }
-
         
         for (int i = 0; i < t.length(); i++) {
             count[t.charAt(i) - 'a']--;
         }
-
-        
+                
         for (int i = 0; i < 26; i++) {
             if (count[i] != 0) {
                 return false;
