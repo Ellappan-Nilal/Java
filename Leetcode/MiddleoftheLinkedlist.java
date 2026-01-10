6e1fd587a2adfa230fdf1b3c8f9882246a10ab6b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-10 21:09:17 +0530

    File is chnage

--- Leetcode/MiddleoftheLinkedlist.java
+++ Leetcode/MiddleoftheLinkedlist.java
@@ -30,7 +30,6 @@ public class MiddleoftheLinkedlist {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-
         while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
