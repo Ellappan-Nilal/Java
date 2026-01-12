5a224e08a7aade4b8ec1be6f9bb55e7ae97c0a69
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-12 20:13:57 +0530

    File is change

--- Leetcode/MoveZeros.java
+++ Leetcode/MoveZeros.java
@@ -6,14 +6,9 @@ public class MoveZeros {
 
     public static void main(String[] args) {
 
-        MoveZeros obj = new MoveZeros();
-
-        
+        MoveZeros obj = new MoveZeros();        
         int[] nums = {0, 1, 0, 3, 12};
-
-        obj.moveZeroes(nums);
-
-        
+        obj.moveZeroes(nums);  
         System.out.println(Arrays.toString(nums)); // [1, 3, 12, 0, 0]
     }
 
