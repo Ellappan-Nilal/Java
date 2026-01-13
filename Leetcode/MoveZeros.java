b0f7bfe4523b1af4c3d1e7334d86e9655b9c6442
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-13 19:01:59 +0530

    File is chnage

--- Leetcode/MoveZeros.java
+++ Leetcode/MoveZeros.java
@@ -11,9 +11,7 @@ public class MoveZeros {
         System.out.println(Arrays.toString(nums)); // [1, 3, 12, 0, 0]
     }
     public void moveZeroes(int[] nums) {
-        int index = 0;
-
-        
+        int index = 0;        
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
                 nums[index] = nums[i];
