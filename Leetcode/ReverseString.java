cfdedcc8a7e35a6069508487df83af3a07e12051
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-01-14 21:34:45 +0530

    FIle is change

--- Leetcode/ReverseString.java
+++ Leetcode/ReverseString.java
@@ -3,16 +3,11 @@ package solve_problems.Leetcode;
 import java.util.Arrays;
 
 public class ReverseString {
-
     public static void main(String[] args) {
         ReverseString solution = new ReverseString();
-
         char[] s = {'h', 'e', 'l', 'l', 'o'};
-
         solution.reverseString(s);
-
-        System.out.println(Arrays.toString(s));
-        // Output: [o, l, l, e, h]
+        System.out.println(Arrays.toString(s));        // Output: [o, l, l, e, h]
     }
 
     public void reverseString(char[] s) {
