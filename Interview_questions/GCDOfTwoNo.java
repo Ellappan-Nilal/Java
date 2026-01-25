39195d8cc8ca004a111628d0d049dde948e6fcc6
Ellappan P <anbunilalkumar@gmail.com>
2026-01-25 21:44:59 +0530

    Remove unnecessary blank lines in GCDOfTwoNo.java

--- Interview_questions/GCDOfTwoNo.java
+++ Interview_questions/GCDOfTwoNo.java
@@ -1,8 +1,5 @@
-
 package solve_problems.Interview_questions;
-
 import java.util.Scanner;
-
 public class GCDOfTwoNo {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
