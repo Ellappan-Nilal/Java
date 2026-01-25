00ff7a2ae41ffef76eff0e202563fba76c9003f5
Ellappan P <anbunilalkumar@gmail.com>
2026-01-25 21:45:51 +0530

    Refactor GCD function for clarity and efficiency

--- Interview_questions/GCDOfTwoNo.java
+++ Interview_questions/GCDOfTwoNo.java
@@ -13,12 +13,10 @@ public class GCDOfTwoNo {
             return b;
         if (b == 0)
             return a;
-
-        
+       
         if (a == b)
             return a;
-
-        
+       
         if (a > b)
             return gcd(a - b, b);
         return gcd(a, b - a);
