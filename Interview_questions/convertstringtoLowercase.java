7a2e3dcf2fd9b98de571e2a7818776b4872693d1
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-04 11:58:17 +0530

    File code is added

--- Interview_questions/convertstringtoLowercase.java
+++ Interview_questions/convertstringtoLowercase.java
@@ -1,5 +1,23 @@
 package Interview_questions;
 
 public class convertstringtoLowercase {
+    public static void main(String[] args) {
+        convertstringtoLowercase obj = new convertstringtoLowercase();
+        String str = "HeLLo WoRLd!";
+        String result = obj.toLowerCase(str);
 
+        System.out.println("Lowercase string: " + result);
+    }
+
+    public String toLowerCase(String str) {
+        StringBuilder lowerStr = new StringBuilder();
+        for (char ch : str.toCharArray()) {
+            if (ch >= 'A' && ch <= 'Z') {
+                lowerStr.append((char) (ch + 32));
+            } else {
+                lowerStr.append(ch);
+            }
+        }
+        return lowerStr.toString();
+    }
 }
