8219a91fc00bb604e2846e3f929384da4e8ddcea
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-06 19:31:47 +0530

    File is change

--- Interview_questions/Quadratic_equation.java
+++ Interview_questions/Quadratic_equation.java
@@ -1,5 +1,29 @@
 package Interview_questions;
-
+import java.util.Scanner;
 public class Quadratic_equation {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        double discriminant = (b * b) - (4 * a * c);
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println(root1);
+            System.out.println(root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println(root);
+        } else {
+            System.out.println("The equation has no real root");
+        }
+
+        sc.close();
+    }
 }
+
+
