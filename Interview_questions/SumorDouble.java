e9969c0a1c25a16116691a3db3489995dec22cbf
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-06 19:23:30 +0530

    FIle is change

--- Interview_questions/SumorDouble.java
+++ Interview_questions/SumorDouble.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class SumorDouble {
-    
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int a=sc.nextInt();
+         int b=sc.nextInt();
+         int sum=a+b;
+         if(a==b){
+             System.out.println(sum);
+         }else{
+             System.out.println(sum*2);
+       }
+	}
 }
