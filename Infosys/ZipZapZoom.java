907c3cdae091357c025870a0fc983d3dcbc5bec7
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-09 18:41:43 +0530

    File is added

--- Infosys/ZipZapZoom.java
+++ Infosys/ZipZapZoom.java
@@ -1,5 +1,24 @@
 package Infosys;
 
-public class ZipZapZoom {
+import java.util.Scanner;
 
+class ZipZapZoom {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 3 == 0 && n % 5 == 0) {
+            System.out.println("Zoom");
+        } 
+        else if (n % 3 == 0) {
+            System.out.println("Zip");
+        } 
+        else if (n % 5 == 0) {
+            System.out.println("Zap");
+        } 
+        else {
+            System.out.println("Invalid");
+        }
+    }
 }
