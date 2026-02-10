7e04c512fb2765416ca735e5927967f3a1ae19e3
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-10 22:06:37 +0530

    File is change

--- Infosys/ZipZapZoom.java
+++ Infosys/ZipZapZoom.java
@@ -4,13 +4,10 @@ import java.util.Scanner;
 
 class ZipZapZoom {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
         if (n % 3 == 0 && n % 5 == 0) {
-            System.out.println("Zoom");
-        } 
+            System.out.println("Zoom");        } 
         else if (n % 3 == 0) {
             System.out.println("Zip");
         } 
