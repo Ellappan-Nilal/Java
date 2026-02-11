254fbb9a921a5f7b0dca6263d891c4ce9805431a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-11 18:43:34 +0530

    File is added

--- Infosys/Seedvaluemultiple.java
+++ Infosys/Seedvaluemultiple.java
@@ -1,5 +1,29 @@
 package Infosys;
-
+import java.util.Scanner;
 public class Seedvaluemultiple {
 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();   // Seed number
+        int y = sc.nextInt();   // Target number
+        
+        int temp = x;
+        int product = x;
+
+        // Multiply x with each of its digits
+        while (temp != 0) {
+            product *= (temp % 10);
+            temp /= 10;
+        }
+
+        if (product == y) {
+            System.out.println(x + " is a seed of " + y);
+        } else {
+            System.out.println(x + " is not a seed of " + y);
+        }
+
+        sc.close();
+    }
 }
+
