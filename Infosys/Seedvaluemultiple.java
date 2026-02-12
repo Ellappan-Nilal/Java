b3828bcd08b042ed7b3bc90d220eedfeda00fb8b
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-12 22:45:23 +0530

    File is change

--- Infosys/Seedvaluemultiple.java
+++ Infosys/Seedvaluemultiple.java
@@ -6,11 +6,9 @@ public class Seedvaluemultiple {
         Scanner sc = new Scanner(System.in);
 
         int x = sc.nextInt();   // Seed number
-        int y = sc.nextInt();   // Target number
-        
+        int y = sc.nextInt();   // Target number        
         int temp = x;
         int product = x;
-
         // Multiply x with each of its digits
         while (temp != 0) {
             product *= (temp % 10);
