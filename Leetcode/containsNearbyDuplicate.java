3fba56b45f35bbd18e5d360b2349fcb124d54077
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-13 22:02:15 +0530

    FIle is added

---
+++ Leetcode/containsNearbyDuplicate.java
@@ -0,0 +1,26 @@
+public class containsNearbyDuplicate {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        java.util.HashSet<Integer> set = new java.util.HashSet<>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            }
+            set.add(nums[i]);
+            
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+    
+    public static void main(String[] args) {
+        containsNearbyDuplicate solution = new containsNearbyDuplicate();
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+        boolean result = solution.containsNearbyDuplicate(nums, k);
+        System.out.println(result); // Output: true
+    }
+}
