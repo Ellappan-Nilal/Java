b74b9afd7619ceb6a9f114604fcb636f3bcb0866
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-13 21:44:22 +0530

    File is added

---
+++ Leetcode/TruncateSentence.java
@@ -0,0 +1,15 @@
+public class TruncateSentence {
+    public String truncateSentence(String s, int k) {
+        String[] words = s.split(" ");
+        StringBuilder truncated = new StringBuilder();
+        
+        for (int i = 0; i < k && i < words.length; i++) {
+            truncated.append(words[i]);
+            if (i < k - 1 && i < words.length - 1) {
+                truncated.append(" ");
+            }
+        }
+        
+        return truncated.toString();
+    }
+}
