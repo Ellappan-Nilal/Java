1b35361dac280fe511134470c3b2f7410aee5339
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-14 21:28:35 +0530

    File is change

--- Interview_questions/EvenIndexCharacters.java
+++ Interview_questions/EvenIndexCharacters.java
@@ -1,3 +1,14 @@
-public class EvenIndexCharacters {
+import java.util.Scanner;
 
+public class EvenIndexCharacters {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        for (int i = 0; i < s.length(); i += 2) {
+            System.out.print(s.charAt(i));
+        }
+        sc.close();
+    }
 }
