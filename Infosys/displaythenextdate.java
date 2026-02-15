db9fe4f84d5319ab387822c23a175e2b596c4b4f
Ellappan P <anbunilalkumar@gmail.com>
2026-02-15 21:47:32 +0530

    Remove unnecessary blank lines in displaythenextdate.java

--- Infosys/displaythenextdate.java
+++ Infosys/displaythenextdate.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
-
 class displaythenextdate {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
 
         int d = sc.nextInt();
