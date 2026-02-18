36888d0d1268e3d0041addef91610361ef650295
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-18 22:25:58 +0530

    File is change

---
+++ Pattern/Character/PrintSquare.java
@@ -0,0 +1,24 @@
+package Pattern.Character;
+
+public class PrintSquare {
+    
+    public static void main(String[] args) {
+        int size = 5; // Size of the square
+        printSquare(size);
+    }
+
+    public static void printSquare(int n) {
+         for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+
+            }
+            System.out.println();
+        }
+        
+}
