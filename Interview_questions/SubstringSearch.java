9c3247b3afd5be391ac1b66e54a9f5cd2999b077
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-21 22:38:14 +0530

    File is added

---
+++ Interview_questions/SubstringSearch.java
@@ -0,0 +1,13 @@
+public class SubstringSearch {
+    public static void main(String[] args) {
+        String str = "Hello, welcome to the world of Java programming!";
+        String substring = "world";
+
+        int index = str.indexOf(substring);
+        if (index != -1) {
+            System.out.println("Substring '" + substring + "' found at index: " + index);
+        } else {
+            System.out.println("Substring '" + substring + "' not found.");
+        }
+    }
+}
