8c7d0d367b36b2d4f25e7bb6d4aa9b9a229073a3
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-24 18:35:15 +0530

    File is added

--- Interview_questions/Luckyno.java
+++ Interview_questions/Luckyno.java
@@ -1,3 +1,26 @@
 public class Luckyno {
+    public static void main(String[] args) {
+        
+        int number = 1623;   // You can change this number
+        int sum = 0;
+        int position = 1;
 
+        while (number > 0) {
+            int digit = number % 10;
+
+            if (position % 2 == 0) {   // Even position
+                sum += digit * digit;
+            }
+
+            number = number / 10;
+            position++;
+        }
+
+        if (sum % 9 == 0) {
+            System.out.println("Lucky Number");
+        } else {
+            System.out.println("Not a Lucky Number");
+        }
+    }
 }
+
