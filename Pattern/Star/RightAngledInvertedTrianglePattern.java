dbc2d51456fdc3490fcdeed977058f77fc59beda
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-02-24 19:05:29 +0530

    File is added

--- Pattern/Star/RightAngledInvertedTrianglePattern.java
+++ Pattern/Star/RightAngledInvertedTrianglePattern.java
@@ -1,5 +1,14 @@
 package Pattern.Star;
 
-public class RightAngledInvertedTrianglePattern {
+import java.util.Scanner;
 
+public class RightAngledInvertedTrianglePattern {
+    Scanner sc=new Scanner(System.in);
+    int n=sc.nextInt();
+    for(int i=n;i>0;i--){
+        for(int j=1;j<i;j++){
+            System.out.println(i);
+        }
+        System.out.println();
+    }
 }
