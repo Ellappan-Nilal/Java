c9ce7e9ea3b32ef10fe31ef63a13df74ea939fa5
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-03-01 21:08:38 +0530

    Code is added

--- Interview_questions/sumofleftdiagonal.java
+++ Interview_questions/sumofleftdiagonal.java
@@ -1,5 +1,18 @@
 package Interview_questions;
 
 public class sumofleftdiagonal {
-
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+        
+        int sum = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            sum += matrix[i][i]; // Adding the left diagonal elements
+        }
+        
+        System.out.println("Sum of left diagonal: " + sum);
+    }
 }
