02cf0d88eebf5657383a0490cec5c554136408aa
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-03-01 21:11:31 +0530

    File is added

--- Interview_questions/SubstringRange.java
+++ Interview_questions/SubstringRange.java
@@ -1,5 +1,32 @@
 package Interview_questions;
-
+import java.util.Scanner;
 public class SubstringRange {
 
+    static String javaSub(String S, int L, int R) {
+        
+        String result = "";
+        
+        for(int i = L; i <= R; i++) {
+            result = result + S.charAt(i);
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Input string
+        String S = sc.nextLine();
+        
+        // Input L and R
+        int L = sc.nextInt();
+        int R = sc.nextInt();
+        
+        // Call function
+        String output = javaSub(S, L, R);
+        
+        // Print result
+        System.out.println(output);
+    }
 }
