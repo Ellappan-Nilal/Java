c8964cc836e5e1e239fcf38e8579400b8e401f73
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-03-01 20:59:16 +0530

    File is added

--- Interview_questions/FibonacciNumber.java
+++ Interview_questions/FibonacciNumber.java
@@ -9,21 +9,18 @@ public class FibonacciNumber
         if (n == 0) {
             System.out.println(0);
             return;
-        }
-        
+        }        
         if (n == 1) {
             System.out.println(1);
             return;
-        }
-        
+        }        
         int a = 0, b = 1, c = 0;
         
         for (int i = 2; i <= n; i++) {
             c = a + b;
             a = b;
             b = c;
-        }
-        
+        }        
         System.out.println(c);
     }
 }
