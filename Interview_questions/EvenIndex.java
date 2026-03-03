a2305ab53f6f7bcb517f2d22f414458c5f8dde95
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-03-03 18:26:59 +0530

    file code is added

--- Interview_questions/EvenIndex.java
+++ Interview_questions/EvenIndex.java
@@ -1,3 +1,29 @@
-public class EvenIndex {
+import java.util.Scanner;
 
+class EvenIndex {
+    static String evenIndex(String s) {
+        
+        String result = "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            if(i % 2 == 0) {
+                result = result + s.charAt(i);
+            }
+        }
+        
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        // Input string
+        String s = sc.nextLine();
+        
+        // Call function
+        String output = evenIndex(s);
+        
+        // Print result
+        System.out.println(output);
+    }
 }
