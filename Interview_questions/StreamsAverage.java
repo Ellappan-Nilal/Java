73f8bc1794eacb2481ff1f1dbb6cb1d0f07dde94
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-03-05 19:02:43 +0530

    File code is added

--- Interview_questions/StreamsAverage.java
+++ Interview_questions/StreamsAverage.java
@@ -1,5 +1,20 @@
 package Interview_questions;
 
 public class StreamsAverage {
+public static double findAverage(int[] arr) {
+        int sum = 0;
 
+        for(int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return (double) sum / arr.length;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+
+        double avg = findAverage(arr);
+        System.out.println(avg);
+    }
 }
