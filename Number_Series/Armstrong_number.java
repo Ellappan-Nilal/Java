7197f498f70f246eff3db69d0afc8d81a302b57d
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-07-17 10:47:30 +0530

    File code is added

--- Number_Series/Armstrong_number.java
+++ Number_Series/Armstrong_number.java
@@ -1,6 +1,22 @@
 package solve_problems.Number_Series;
-
-
+import java.util.Scanner;
 public class Armstrong_number {
-    
+   public static void main(String args[]) {
+      int num , originalNum, remainder, result = 0;
+      Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        num = sc.nextInt();
+      originalNum = num;
+
+      while (originalNum != 0) {
+         remainder = originalNum % 10;
+         result += Math.pow(remainder, 3);
+         originalNum /= 10;
+      }
+
+      if (result == num)
+         System.out.println(num + " is an Armstrong number.");
+      else
+         System.out.println(num + " is not an Armstrong number.");
+   }
 }
