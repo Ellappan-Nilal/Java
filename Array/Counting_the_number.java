ddac45f6a8ab8a06ea224d356d8d68727338a46a
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-08-07 05:54:17 +0530

    File is change

--- Array/Counting_the_number.java
+++ Array/Counting_the_number.java
@@ -3,6 +3,7 @@ public class Counting_the_number
 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
+                System.out.println("Enter the no count value :");
 		int n=sc.nextInt();
 		int[] a=new int[n];
 		for(int i=0; i<n; i++) {
--- Array/Missing_array.java
+++ Array/Missing_array.java
@@ -1,4 +1,19 @@
 import Java.util.*;
 public class Missing_array {
-
+    Scanner sc=new Scanner(System.in);
+    public void missing(){
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        for(int i=0;i<n-1;i++){
+            if(arr[i+1]-arr[i]>1){
+                for(int j=arr[i]+1;j<arr[i+1];j++){
+                    System.out.print(j+" ");
+                }
+            }
+        }
+    }
 }
