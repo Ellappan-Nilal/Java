005276638fcf9e1dd468f7cc5a324d745e89fcbe
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-08-10 10:22:21 +0530

    percentage file is added

--- Java_class_Anudhip/Primitive_data_types.java
+++ Java_class_Anudhip/Primitive_data_types.java
@@ -1,4 +1,4 @@
-
+import java.util.*;
 public class Primitive_data_types {
 
     public static void main(String[] args) {
@@ -12,14 +12,14 @@ public class Primitive_data_types {
         char c = 'A';
         boolean flag = true;
 
-        System.out.println("byte = " + b);
-        System.out.println("short = " + s);
-        System.out.println("int = " + i);
-        System.out.println("long = " + l);
-        System.out.println("float = " + f);
-        System.out.println("double = " + d);
-        System.out.println("char = " + c);
-        System.out.println("boolean = " + flag);
+        System.out.println("byte =      " + b);
+        System.out.println("short =     " + s);
+        System.out.println("int =       " + i);
+        System.out.println("long =      " + l);
+        System.out.println("float =     " + f);
+        System.out.println("double =    " + d);
+        System.out.println("char =      " + c);
+        System.out.println("boolean =   " + flag);
     }
 }
 
---
+++ Java_class_Anudhip/productandGSTpercentage.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class productandGSTpercentage {
+    public static void main(String[] args) {
+
+        double price = 1000.00;
+        double gst = 18.0;
+
+        System.out.println("Product Price = ₹" + price);
+        System.out.println("GST Percentage = " + gst + "%");
+    }
+}
