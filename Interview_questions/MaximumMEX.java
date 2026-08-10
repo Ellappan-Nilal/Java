186eeedb52578333413b1ac333fba342193a88af
Ellappan-Nilal <anbunilalkumar@gmail.com>
2026-08-10 07:58:44 +0530

    Code instrection is added

--- Interview_questions/MaximumMEX.java
+++ Interview_questions/MaximumMEX.java
@@ -5,9 +5,9 @@ public class MaximumMEX {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        System.out.print("Enter the number of elements in the array: ");
         int n = sc.nextInt();
-
+        System.out.print("Enter the elements of the array: ");
         int[] a = new int[n];
 
         for (int i = 0; i < n; i++) {
